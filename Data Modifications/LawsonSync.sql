085085cd42e4ac61d4cec2ea17e73c9e5a02e521
Debbie Chavez <Chavez_Deb@aps.edu>
2014-04-28 19:46:35 +0000

    Removed Subselect in FROM and added where conditions
    git-svn-id: file:///var/repos/aps/Synergy/trunk@24 74b2d9da-2730-d14b-887d-b15a5e3d2562

--- Data Modifications/LawsonSync.sql
+++ Data Modifications/LawsonSync.sql
@@ -20,127 +20,109 @@ GO
 BEGIN TRANSACTION
 
 UPDATE 
-	ST_Stars.rev.REV_PERSON
+	PERSON
 SET		
-	GENDER = UPDATEPERSON.LAW_GENDER
-	, BIRTH_DATE = UPDATEPERSON.LAW_BIRTHDATE
-	, ETHNIC_CODE = UPDATEPERSON.LAW_ETHNIC_CODE
-	, HISPANIC_INDICATOR = UPDATEPERSON.LAW_HISPANIC_INDICATOR
-	, PRIMARY_PHONE = UPDATEPERSON.LAW_PHONE
+	GENDER = LAWSON.LAW_GENDER
+	, BIRTH_DATE = LAWSON.LAW_BIRTHDATE
+	, ETHNIC_CODE = LAWSON.LAW_ETHNIC_CODE
+	, HISPANIC_INDICATOR = LAWSON.LAW_HISPANIC_INDICATOR
+	, PRIMARY_PHONE = LAWSON.LAW_PHONE
 	, CHANGE_DATE_TIME_STAMP = GETDATE()
 	, CHANGE_ID_STAMP = '27CDCD0E-BF93-4071-94B2-5DB792BB735F'
-		
-/*
-SELECT 
-	* 
-*/	
-	
-FROM 
 
-(
-	SELECT 
-		LAWSON.*
-		, STAFF.STAFF_GU
-	FROM
-		--ONLY UPDATE STAFF AND PERSON RECORDS THAT ARE NOT DISABLED IN SYNERGY (ACTIVE STAFF ONLY)
-		rev.EPC_STAFF AS STAFF
-		
-		INNER JOIN
-		rev.REV_USER AS USERS
-		ON 
-		USERS.USER_GU = STAFF.STAFF_GU
-		AND DISABLED = 'N'
+FROM
+	--ONLY UPDATE STAFF AND PERSON RECORDS THAT ARE NOT DISABLED IN SYNERGY (ACTIVE STAFF ONLY)
+	rev.EPC_STAFF AS STAFF
+	INNER JOIN
+	rev.REV_USER AS USERS
+	ON 
+	USERS.USER_GU = STAFF.STAFF_GU
+	INNER JOIN
+	rev.REV_PERSON AS PERSON
+	ON 
+	STAFF_GU = PERSON_GU
 	
-		INNER JOIN
-			( 
-				SELECT 
-					*
+	INNER HASH JOIN
+		( 
+			SELECT 
+				*
+			FROM
+				(
+				SELECT
+					-------------------------------------------------------------------------------------------------------------------------------
+					--FIELDS NEEDED FOR REV.PERSON IN SYNERGY
+					-- APS.FormatTitleCase(Employee.LAST_NAME) AS LAW_LAST_NAME
+					--,APS.FormatTitleCase(Employee.FIRST_NAME) AS LAW_FIRST_NAME
+					--,APS.FormatTitleCase(Employee.MIDDLE_NAME) AS LAW_MIDDLE_NAME
+					CASE WHEN Paemployee.SEX NOT IN('M', 'F') THEN '' ELSE Paemployee.SEX END AS LAW_GENDER 
+					,CASE WHEN Paemployee.EEO_CLASS = 'HI' THEN 'Y' ELSE 'N' END AS LAW_HISPANIC_INDICATOR 
+					
+					,CASE WHEN Paemployee.EEO_CLASS = 'WH' THEN '1'
+						  WHEN Paemployee.EEO_CLASS = 'AA' THEN '100'
+						  WHEN Paemployee.EEO_CLASS = 'AP' THEN '299'
+						  WHEN Paemployee.EEO_CLASS = 'BL' THEN '600'
+						  WHEN Paemployee.EEO_CLASS = 'HI' THEN '1'
+					ELSE ''	END	 AS LAW_ETHNIC_CODE 
+					
+					,Paemployee.BIRTHDATE AS LAW_BIRTHDATE
+					--,Employee.EMAIL_ADDRESS AS LAW_EMAIL
+					,LEFT(REPLACE(REPLACE(Paemployee.HM_PHONE_NBR,'.', ''),'-', ''),10) AS LAW_PHONE  
+					--------------------------------------------------------------------------------------------------------------------------------					
+					,ROW_NUMBER() OVER (PARTITION BY Paemppos.COMPANY, Paemppos.EMPLOYEE ORDER BY Paemppos.FTE DESC, Paemppos.EFFECT_DATE ASC) AS RN
+					,'e' + RIGHT('000000'+ CONVERT (VARCHAR (6), CAST(Employee.EMPLOYEE AS VARCHAR)), 6) AS LAW_BADGE_NUMBER 
+					
 				FROM
-					(
-					SELECT
-						-------------------------------------------------------------------------------------------------------------------------------
-						--FIELDS NEEDED FOR REV.PERSON IN SYNERGY
-						-- APS.FormatTitleCase(Employee.LAST_NAME) AS LAW_LAST_NAME
-						--,APS.FormatTitleCase(Employee.FIRST_NAME) AS LAW_FIRST_NAME
-						--,APS.FormatTitleCase(Employee.MIDDLE_NAME) AS LAW_MIDDLE_NAME
-						CASE WHEN Paemployee.SEX NOT IN('M', 'F') THEN '' ELSE Paemployee.SEX END AS LAW_GENDER 
-						,CASE WHEN Paemployee.EEO_CLASS = 'HI' THEN 'Y' ELSE 'N' END AS LAW_HISPANIC_INDICATOR 
-						
-						,CASE WHEN Paemployee.EEO_CLASS = 'WH' THEN '1'
-							  WHEN Paemployee.EEO_CLASS = 'AA' THEN '100'
-							  WHEN Paemployee.EEO_CLASS = 'AP' THEN '299'
-							  WHEN Paemployee.EEO_CLASS = 'BL' THEN '600'
-							  WHEN Paemployee.EEO_CLASS = 'HI' THEN '1'
-						ELSE ''	END	 AS LAW_ETHNIC_CODE 
-						
-						,Paemployee.BIRTHDATE AS LAW_BIRTHDATE
-						--,Employee.EMAIL_ADDRESS AS LAW_EMAIL
-						,LEFT(REPLACE(REPLACE(Paemployee.HM_PHONE_NBR,'.', ''),'-', ''),10) AS LAW_PHONE  
-						--------------------------------------------------------------------------------------------------------------------------------					
-						,ROW_NUMBER() OVER (PARTITION BY Paemppos.COMPANY, Paemppos.EMPLOYEE ORDER BY Paemppos.FTE DESC, Paemppos.EFFECT_DATE ASC) AS RN
-						,'e' + RIGHT('000000'+ CONVERT (VARCHAR (6), CAST(Employee.EMPLOYEE AS VARCHAR)), 6) AS LAW_BADGE_NUMBER 
-						
-					FROM
-						[180-SMAXODS-01].Lawson.dbo.Paemppos
-						INNER JOIN
-						[180-SMAXODS-01].Lawson.dbo.Employee
-						ON
-						Paemppos.EMPLOYEE = Employee.EMPLOYEE	
-						INNER JOIN
-						[180-SMAXODS-01].Lawson.dbo.Paemployee
-						ON
-						Paemppos.EMPLOYEE = Paemployee.EMPLOYEE	
-					) AS LAW
-			WHERE
-				RN = 1
-			) AS LAWSON
+					[180-SMAXODS-01].Lawson.dbo.Paemppos
+					INNER JOIN
+					[180-SMAXODS-01].Lawson.dbo.Employee
+					ON
+					Paemppos.EMPLOYEE = Employee.EMPLOYEE	
+					INNER JOIN
+					[180-SMAXODS-01].Lawson.dbo.Paemployee
+					ON
+					Paemppos.EMPLOYEE = Paemployee.EMPLOYEE	
+				) AS LAW
+		WHERE
+			RN = 1
+		) AS LAWSON
 
-		ON
-		STAFF.BADGE_NUM = LAWSON.LAW_BADGE_NUMBER
-
-) AS UPDATEPERSON
+	ON
+	STAFF.BADGE_NUM = LAWSON.LAW_BADGE_NUMBER
 
 WHERE
-	PERSON_GU = UPDATEPERSON.STAFF_GU
+	DISABLED = 'N'
+	AND EXEMPT_FROM_LDAP = 'N'
+	AND BADGE_NUM BETWEEN 'e205300' AND 'e205352'
+	
+	
 
 ROLLBACK
 
 ----------------------------------------------------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------------------------------------------
+
 /*
 
 UPDATE 
-	rev.EPC_STAFF
+	STAFF
 SET		
 	STATE_ID = LAW_EMPLOYEE
 	, BADGE_NUM = LAW_BADGE_NUMBER
 	, HIRE_DATE = LAW_HIRE_DATE
 	, TYPE = LAW_TYPE
+	, CHANGE_DATE_TIME_STAMP = GETDATE()
 	, CHANGE_ID_STAMP = '27CDCD0E-BF93-4071-94B2-5DB792BB735F'
 
-/* 
 
-
-SELECT 
-	* 
-*/
-
-FROM 
-
-(
-	SELECT 
-		LAWSON.*
-		,STAFF.STAFF_GU
 	FROM
 		--ONLY UPDATE STAFF AND PERSON RECORDS THAT ARE NOT DISABLED IN SYNERGY (ACTIVE STAFF ONLY)
 		rev.EPC_STAFF AS STAFF
-	
 		INNER JOIN
 		rev.REV_USER AS USERS
 		ON 
 		USERS.USER_GU = STAFF.STAFF_GU
-		AND DISABLED = 'N'
+
 	
 		INNER JOIN
 			( 
@@ -191,9 +173,9 @@ FROM
 		ON
 		STAFF.BADGE_NUM = LAWSON.LAW_BADGE_NUMBER
 
-) AS UDPATESTAFF
-
 WHERE
-	STAFF_GU = UDPATESTAFF.STAFF_GU
+	DISABLED = 'N'
+	AND EXEMPT_FROM_LDAP = 'N'
 
 */
+
