2a70cd3c4cbd9526b5a0df5364c939c048208c8c
Debbie Chavez <Chavez_Deb@aps.edu>
2014-07-11 20:40:10 +0000

    *Count of Users
    git-svn-id: file:///var/repos/aps/Synergy/trunk@53 74b2d9da-2730-d14b-887d-b15a5e3d2562

---
+++ Queries/By Area/User/CountOfUsers.sql
@@ -0,0 +1,13 @@
+/*
+*	Created By:  Debbie Ann Chavez
+*	Date:  7/11/2014
+*
+*	This is a super awesome query to count users that will be run daily on PRTG.
+*	
+*/
+
+
+SELECT 
+	COUNT (*)
+FROM
+	rev.REV_USER
