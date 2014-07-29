6b80c65b20e152f533f19a64110a5216d37134e1
Gary Corbaley <Gary.Corbaley@aps.edu>
2014-07-29 21:18:41 +0000

    Added a code crosswalk to all 5 language questions.
    git-svn-id: file:///var/repos/aps/Synergy/trunk@66 74b2d9da-2730-d14b-887d-b15a5e3d2562

--- User/Gary Corbaley/2014 PHLOTE test scores and language codes.sql
+++ User/Gary Corbaley/2014 PHLOTE test scores and language codes.sql
@@ -87,11 +87,16 @@ SELECT
 	,[PrimaryLanguageCodeCrosswalk].[SYNERGY_CODE] AS [Primary Language Synergy]
 	,[Languages].[CON_LNG] AS [Contact Language SMAX]
 	,[ContactLanguageCodeCrosswalk].[SYNERGY_CODE] AS [Contact Language Synergy]
-	,[Languages].[HLS_Q_1]
-	,[Languages].[HLS_Q_2]
-	,[Languages].[HLS_Q_3_1]
-	,[Languages].[HLS_Q_4_1]
-	,[Languages].[HLS_Q_5_1]
+	,[Languages].[HLS_Q_1] AS [QUESTION 1 SMAX]
+	,[Test1LanguageCodeCrosswalk].[SYNERGY_CODE] AS [QUESTION 1 SYNERGY]
+	,[Languages].[HLS_Q_2] AS [QUESTION 2 SMAX]
+	,[Test2LanguageCodeCrosswalk].[SYNERGY_CODE] AS [QUESTION 2 SYNERGY]
+	,[Languages].[HLS_Q_3_1] AS [QUESTION 3 SMAX]
+	,[Test3LanguageCodeCrosswalk].[SYNERGY_CODE] AS [QUESTION 3 SYNERGY]
+	,[Languages].[HLS_Q_4_1] AS [QUESTION 4 SMAX]
+	,[Test4LanguageCodeCrosswalk].[SYNERGY_CODE] AS [QUESTION 4 SYNERGY]
+	,[Languages].[HLS_Q_5_1] AS [QUESTION 5 SMAX]
+	,[Test5LanguageCodeCrosswalk].[SYNERGY_CODE] AS [QUESTION 5 SYNERGY]
 	
 FROM
 	(
@@ -112,7 +117,37 @@ FROM
 	@LanguageCodeCrosswalk AS [ContactLanguageCodeCrosswalk]
 	
 	ON
-	[Languages].[PRM_LNG] = [ContactLanguageCodeCrosswalk].[SMAX_CODE]
+	[Languages].[CON_LNG] = [ContactLanguageCodeCrosswalk].[SMAX_CODE]
+	
+	INNER JOIN
+	@LanguageCodeCrosswalk AS [Test1LanguageCodeCrosswalk]
+	
+	ON
+	[Languages].[HLS_Q_1] = [Test1LanguageCodeCrosswalk].[SMAX_CODE]
+	
+	INNER JOIN
+	@LanguageCodeCrosswalk AS [Test2LanguageCodeCrosswalk]
+	
+	ON
+	[Languages].[HLS_Q_2] = [Test2LanguageCodeCrosswalk].[SMAX_CODE]
+	
+	INNER JOIN
+	@LanguageCodeCrosswalk AS [Test3LanguageCodeCrosswalk]
+	
+	ON
+	[Languages].[HLS_Q_3_1] = [Test3LanguageCodeCrosswalk].[SMAX_CODE]
+	
+	INNER JOIN
+	@LanguageCodeCrosswalk AS [Test4LanguageCodeCrosswalk]
+	
+	ON
+	[Languages].[HLS_Q_4_1] = [Test4LanguageCodeCrosswalk].[SMAX_CODE]
+	
+	INNER JOIN
+	@LanguageCodeCrosswalk AS [Test5LanguageCodeCrosswalk]
+	
+	ON
+	[Languages].[HLS_Q_5_1] = [Test5LanguageCodeCrosswalk].[SMAX_CODE]
 	
 WHERE
 	[Languages].[DST_NBR] = 1
