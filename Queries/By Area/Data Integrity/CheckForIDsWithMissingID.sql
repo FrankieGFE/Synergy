c3b8e10319d5d287d0c23f43d130cea1d858550d
Donald J Jarrett <Donald.Jarrett@aps.edu>
2014-07-29 20:23:40 +0000

    * Initial commit of CheckForIDsWithMissingID.sql.
    git-svn-id: file:///var/repos/aps/Synergy/trunk@63 74b2d9da-2730-d14b-887d-b15a5e3d2562

---
+++ Queries/By Area/Data Integrity/CheckForIDsWithMissingID.sql
@@ -0,0 +1,32 @@
+SELECT
+    *
+FROM
+    (
+	   SELECT
+	   [SMAX].[ID_NBR]
+	   ,[SMAX].[STATE_ID]
+	   ,[Synergy].[SIS_NUMBER]
+	   ,[Synergy].[STATE_STUDENT_NUMBER]
+	   FROM
+	   [DBTSIS].[CE020_V] AS [SMAX]
+
+	   LEFT JOIN
+	   OPENQUERY([011-SYNERGYDB.APS.EDU.ACTD],'SELECT [SIS_NUMBER],[STATE_STUDENT_NUMBER] FROM [ST_Production].[rev].[EPC_STU]') AS [Synergy]
+
+	   ON
+	   CAST([SMAX].[ID_NBR] AS VARCHAR(9))=CAST([Synergy].[SIS_NUMBER] AS VARCHAR(9))
+
+	   WHERE
+	   [Synergy].[SIS_NUMBER] IS NULL
+    ) as [SMAX]
+
+    LEFT JOIN
+    OPENROWSET ('MSDASQL', 'Driver={Microsoft Access Text Driver (*.txt, *.csv)};DBQ=D:\SQLWorkingFiles;', 'SELECT * from "StudentCourseHistory-Additive-7-22.csv"') AS [crs]
+
+    ON 
+    --CAST([SMAX].[ID_NBR] AS VARCHAR(9))=[crs].[SIS_NUMBER]
+    [SMAX].[ID_NBR]=[crs].[SIS_NUMBER]
+
+WHERE
+    [crs].[SIS_NUMBER] IS NOT NULL
+
