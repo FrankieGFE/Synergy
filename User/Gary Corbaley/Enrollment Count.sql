1493c115a7e32a8423f3b208aaa16f6923d5e6a3
Gary Corbaley <Gary.Corbaley@aps.edu>
2014-08-08 20:10:52 +0000

    Changed script to use PrimaryEnrollmentsAsOf() along with the current date.
    git-svn-id: file:///var/repos/aps/Synergy/trunk@78 74b2d9da-2730-d14b-887d-b15a5e3d2562

--- User/Gary Corbaley/Enrollment Count.sql
+++ User/Gary Corbaley/Enrollment Count.sql
@@ -9,59 +9,46 @@
  * This script will get the count of students enrolled in the 2014 school year from the Synergy system.
  */
 
+USE [ST_Experiment]
+
+DECLARE @AsOfDate DATETIME = GETDATE()
+
 SELECT	
 		School.SCHOOL_CODE
 		,Organization.ORGANIZATION_NAME
 		,COUNT (*)AS ENROLL_COUNT
 	FROM
-		[011-SYNERGYDB].ST_Production.rev.EPC_STU_SCH_YR AS StudentSchoolYear
-		INNER JOIN 
-		[011-SYNERGYDB].ST_Production.rev.EPC_STU AS Student
-		ON 
-		Student.STUDENT_GU = StudentSchoolYear.STUDENT_GU
-		INNER JOIN 
-		[011-SYNERGYDB].ST_Production.rev.REV_ORGANIZATION_YEAR AS OrgYear 
-		ON 
-		OrgYear.ORGANIZATION_YEAR_GU = StudentSchoolYear.ORGANIZATION_YEAR_GU
-		INNER JOIN 
-		[011-SYNERGYDB].ST_Production.rev.REV_ORGANIZATION AS Organization 
-		ON 
-		Organization.ORGANIZATION_GU=OrgYear.ORGANIZATION_GU
-		INNER JOIN 
-		[011-SYNERGYDB].ST_Production.rev.EPC_SCH AS School 
-		ON 
-		School.ORGANIZATION_GU =OrgYear.ORGANIZATION_GU
-		INNER JOIN 
-		[011-SYNERGYDB].ST_Production.rev.REV_YEAR AS RevYear 
-		ON 
-		RevYear.YEAR_GU = OrgYear.YEAR_GU 
-		AND RevYear.SCHOOL_YEAR = 2014
-		
-		INNER JOIN 
-		[011-SYNERGYDB].ST_Production.rev.EPC_STU_ENROLL AS Enroll
-		ON 
-		Enroll.STUDENT_SCHOOL_YEAR_GU = StudentSchoolYear.STUDENT_SCHOOL_YEAR_GU
-		
-		INNER JOIN
-        (
-        SELECT
-              Val.[ALT_CODE_1]
-              ,Val.VALUE_CODE
-        FROM
-              [011-SYNERGYDB].ST_Production.[rev].[REV_BOD_LOOKUP_DEF] AS [Def]
-              INNER JOIN
-              [011-SYNERGYDB].ST_Production.[rev].[REV_BOD_LOOKUP_VALUES] AS [Val]
-              ON
-              [Def].[LOOKUP_DEF_GU]=[Val].[LOOKUP_DEF_GU]
-              AND [Def].[LOOKUP_NAMESPACE]='K12'
-              AND [Def].[LOOKUP_DEF_CODE]='Grade'
-        ) AS [Grades]
-        ON
-        Enroll.[GRADE]=[Grades].[VALUE_CODE]
-		
-	WHERE
-		(RevYear.SCHOOL_YEAR = 2014 )--AND RevYear.EXTENSION = 'S')
-		AND SCHOOL_CODE != '592'
+	APS.PrimaryEnrollmentsAsOf(@AsOfDate) AS [EnrollmentsAsOf]
+	
+	INNER JOIN 
+	rev.EPC_STU_ENROLL AS [EnrollmentDetails] -- Contains Grade and Start Date
+	ON 
+	[EnrollmentsAsOf].[ENROLLMENT_GU] = [EnrollmentDetails].[ENROLLMENT_GU]
+	
+	INNER JOIN
+	rev.EPC_STU_SCH_YR AS [StudentSchoolYear] -- Contains Grade and Start Date 	
+	ON
+	[EnrollmentDetails].[STUDENT_SCHOOL_YEAR_GU] = [StudentSchoolYear].[STUDENT_SCHOOL_YEAR_GU]
+	
+	INNER JOIN 
+	rev.REV_ORGANIZATION_YEAR AS [OrgYear] -- Links between School and Year
+	ON 
+	[StudentSchoolYear].[ORGANIZATION_YEAR_GU] = [OrgYear].[ORGANIZATION_YEAR_GU]
+	
+	INNER JOIN 
+	rev.REV_ORGANIZATION AS [Organization] -- Contains the School Name
+	ON 
+	[OrgYear].[ORGANIZATION_GU] = [Organization].[ORGANIZATION_GU]
+	
+	INNER JOIN 
+	rev.REV_YEAR AS [RevYear] -- Contains the School Year
+	ON 
+	[OrgYear].[YEAR_GU] = [RevYear].[YEAR_GU]
+	
+	INNER JOIN 
+	rev.EPC_SCH AS [School] -- Contains the School Code / Number
+	ON 
+	[OrgYear].[ORGANIZATION_GU] = [School].[ORGANIZATION_GU]
 
 GROUP BY 
 	School.SCHOOL_CODE
