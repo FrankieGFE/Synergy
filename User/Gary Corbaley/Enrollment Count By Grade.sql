a117f248018bef2d23c85b2a4acf7656c163c94f
Gary Corbaley <Gary.Corbaley@aps.edu>
2014-08-08 12:56:14 +0000

    Changed script to use APS.EnrollmentsAsOf() to get that days enrolled students.
    git-svn-id: file:///var/repos/aps/Synergy/trunk@76 74b2d9da-2730-d14b-887d-b15a5e3d2562

--- User/Gary Corbaley/Enrollment Count By Grade.sql
+++ User/Gary Corbaley/Enrollment Count By Grade.sql
@@ -1,7 +1,7 @@
 /**
  * 
  * $LastChangedBy: Gary Corbaley
- * $LastChangedDate: 07/22/2014 $
+ * $LastChangedDate: 08/08/2014 $
  *
  * Request By: Andy
  * InitialRequestDate: 7/17/2014
@@ -13,8 +13,8 @@ USE [PR]
  
 --Change the variable @Today if you want to check another member day
 DECLARE 
-		@Today DATE = GETDATE()
-		--@Today DATE = '2014-08-14 00:00:00.000'
+		--@Today DATE = GETDATE()
+		@Today DATE = '2014-08-14 00:00:00.000'
 		,@MemberDay INT 
 		,@CurrentSchoolYear INT
 		,@DayLastYear DATE
@@ -23,6 +23,7 @@ DECLARE
 SET @MemberDay = APS.MemberDayFromDate(1, @Today)
 SET @CurrentSchoolYear = APS.SchoolYearFromDate(@Today)
 SET @DayLastYear = APS.DateFromMemberDayAndSchoolYear(1,@CurrentSchoolYear-1,@MemberDay)
+	
 
 SELECT
 	[SYNERGY_COUNTS].SCHOOL_CODE
@@ -33,67 +34,69 @@ SELECT
 	,[SYNERGY_COUNTS].[ENROLL_COUNT] - CASE WHEN [SMAX_COUNTS].[LastYearCount] IS NULL THEN 0 ELSE [SMAX_COUNTS].[LastYearCount] END AS [Count Difference]
 FROM
 	(
-	SELECT	
-			@Today AS TODAY
-			,School.SCHOOL_CODE
-			,Organization.ORGANIZATION_NAME AS [School]
-			,[Grades].[ALT_CODE_1] AS [Grade]
-			,COUNT (*)AS ENROLL_COUNT
+	SELECT
+		[School].[SCHOOL_CODE]
+		,[Organization].[ORGANIZATION_NAME] AS [School]
+		,[Grades].[ALT_CODE_1] AS [Grade]
+		,COUNT ([EnrollmentsAsOf].[ENROLLMENT_GU]) AS ENROLL_COUNT
+	FROM
+		OPENQUERY([SYNSECONDDB.APS.EDU.ACTD],'SELECT * FROM  [ST_Experiment].APS.PrimaryEnrollmentsAsOf(''08/14/2014'')') AS [EnrollmentsAsOf]
+		
+		INNER JOIN 
+		[SYNSECONDDB.APS.EDU.ACTD].[ST_Experiment].rev.EPC_STU_ENROLL AS [EnrollmentDetails] -- Contains Grade and Start Date
+		ON 
+		[EnrollmentsAsOf].[ENROLLMENT_GU] = [EnrollmentDetails].[ENROLLMENT_GU]
+		
+		INNER JOIN
+		[SYNSECONDDB.APS.EDU.ACTD].[ST_Experiment].rev.EPC_STU_SCH_YR AS [StudentSchoolYear] -- Contains Grade and Start Date 	
+		ON
+		[EnrollmentDetails].[STUDENT_SCHOOL_YEAR_GU] = [StudentSchoolYear].[STUDENT_SCHOOL_YEAR_GU]
+		
+		INNER JOIN 
+		[SYNSECONDDB.APS.EDU.ACTD].[ST_Experiment].rev.REV_ORGANIZATION_YEAR AS [OrgYear] -- Links between School and Year
+		ON 
+		[StudentSchoolYear].[ORGANIZATION_YEAR_GU] = [OrgYear].[ORGANIZATION_YEAR_GU]
+		
+		INNER JOIN 
+		[SYNSECONDDB.APS.EDU.ACTD].[ST_Experiment].rev.REV_ORGANIZATION AS [Organization] -- Contains the School Name
+		ON 
+		[OrgYear].[ORGANIZATION_GU] = [Organization].[ORGANIZATION_GU]
+		
+		INNER JOIN 
+		[SYNSECONDDB.APS.EDU.ACTD].[ST_Experiment].rev.REV_YEAR AS [RevYear] -- Contains the School Year
+		ON 
+		[OrgYear].[YEAR_GU] = [RevYear].[YEAR_GU]
+		
+		INNER JOIN 
+		[SYNSECONDDB.APS.EDU.ACTD].[ST_Experiment].rev.EPC_SCH AS [School] -- Contains the School Code / Number
+		ON 
+		[OrgYear].[ORGANIZATION_GU] = [School].[ORGANIZATION_GU]
+		
+		LEFT OUTER JOIN
+		(
+		SELECT
+			  Val.[ALT_CODE_1]
+			  ,Val.VALUE_CODE
 		FROM
-			[011-SYNERGYDB].ST_Production.rev.EPC_STU_SCH_YR AS StudentSchoolYear
-			INNER JOIN 
-			[011-SYNERGYDB].ST_Production.rev.EPC_STU AS Student
-			ON 
-			Student.STUDENT_GU = StudentSchoolYear.STUDENT_GU
-			INNER JOIN 
-			[011-SYNERGYDB].ST_Production.rev.REV_ORGANIZATION_YEAR AS OrgYear 
-			ON 
-			OrgYear.ORGANIZATION_YEAR_GU = StudentSchoolYear.ORGANIZATION_YEAR_GU
-			INNER JOIN 
-			[011-SYNERGYDB].ST_Production.rev.REV_ORGANIZATION AS Organization 
-			ON 
-			Organization.ORGANIZATION_GU = OrgYear.ORGANIZATION_GU
-			INNER JOIN 
-			[011-SYNERGYDB].ST_Production.rev.EPC_SCH AS School 
-			ON 
-			School.ORGANIZATION_GU = OrgYear.ORGANIZATION_GU
-			INNER JOIN 
-			[011-SYNERGYDB].ST_Production.rev.REV_YEAR AS RevYear 
-			ON 
-			RevYear.YEAR_GU = OrgYear.YEAR_GU 
-			AND RevYear.SCHOOL_YEAR = 2014
-			
-			INNER JOIN 
-			[011-SYNERGYDB].ST_Production.rev.EPC_STU_ENROLL AS Enroll
-			ON 
-			Enroll.STUDENT_SCHOOL_YEAR_GU = StudentSchoolYear.STUDENT_SCHOOL_YEAR_GU
-			
-			INNER JOIN
-			(
-			SELECT
-				  Val.[ALT_CODE_1]
-				  ,Val.VALUE_CODE
-			FROM
-				  [011-SYNERGYDB].ST_Production.[rev].[REV_BOD_LOOKUP_DEF] AS [Def]
-				  INNER JOIN
-				  [011-SYNERGYDB].ST_Production.[rev].[REV_BOD_LOOKUP_VALUES] AS [Val]
-				  ON
-				  [Def].[LOOKUP_DEF_GU]=[Val].[LOOKUP_DEF_GU]
-				  AND [Def].[LOOKUP_NAMESPACE]='K12'
-				  AND [Def].[LOOKUP_DEF_CODE]='Grade'
-			) AS [Grades]
-			ON
-			Enroll.[GRADE]=[Grades].[VALUE_CODE]
-			
-		WHERE
-			(RevYear.SCHOOL_YEAR = 2014 )--AND RevYear.EXTENSION = 'S')
-			AND SCHOOL_CODE != '592'
-			AND StudentSchoolYear.ENTER_DATE <= @Today
-			AND (StudentSchoolYear.LEAVE_DATE IS NULL OR StudentSchoolYear.LEAVE_DATE < @Today)
-
-	GROUP BY 
-		School.SCHOOL_CODE
-		,Organization.ORGANIZATION_NAME
+			  [SYNSECONDDB.APS.EDU.ACTD].[ST_Experiment].[rev].[REV_BOD_LOOKUP_DEF] AS [Def]
+			  INNER JOIN
+			  [SYNSECONDDB.APS.EDU.ACTD].[ST_Experiment].[rev].[REV_BOD_LOOKUP_VALUES] AS [Val]
+			  ON
+			  [Def].[LOOKUP_DEF_GU]=[Val].[LOOKUP_DEF_GU]
+			  AND [Def].[LOOKUP_NAMESPACE]='K12'
+			  AND [Def].[LOOKUP_DEF_CODE]='Grade'
+		) AS [Grades]
+		ON
+		[EnrollmentDetails].[GRADE] = [Grades].[VALUE_CODE]
+	    
+		INNER JOIN 
+		[SYNSECONDDB.APS.EDU.ACTD].[ST_Experiment].rev.EPC_STU AS [Student] -- Contains Student ID State ID Language Code Cohort Year
+		ON 
+		[StudentSchoolYear].[STUDENT_GU] = [Student].[STUDENT_GU]
+	
+	GROUP BY
+		[School].[SCHOOL_CODE]
+		,[Organization].[ORGANIZATION_NAME]
 		,[Grades].[ALT_CODE_1]
 
 	) AS [SYNERGY_COUNTS]
