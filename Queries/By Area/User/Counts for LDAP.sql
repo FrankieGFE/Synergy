a5bec9dd2a19f495e3709df0f76475bb0ceb48b9
Debbie Chavez <Chavez_Deb@aps.edu>
2014-08-22 18:04:48 +0000

    counts for ldap
    git-svn-id: file:///var/repos/aps/Synergy/trunk@100 74b2d9da-2730-d14b-887d-b15a5e3d2562

---
+++ Queries/By Area/User/Counts for LDAP.sql
@@ -0,0 +1,4 @@
+select COUNT(*) as USERS from rev.REV_USER
+select COUNT(*) as Staff from rev.EPC_STAFF
+select COUNT(*) as Staff_Yr from rev.EPC_STAFF_SCH_YR
+select COUNT(*) as User_Group_Members from rev.REV_USER_USERGROUP
