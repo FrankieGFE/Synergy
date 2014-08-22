1d01d989423af6d36a126d7c67b8e6b2bcc093f8
Brian Rieb <Brian.Rieb@aps.edu>
2014-08-22 20:20:27 +0000

    * Woot! First Commit to new server!
    git-svn-id: file:///var/repos/aps/Synergy/trunk@101 74b2d9da-2730-d14b-887d-b15a5e3d2562

---
+++ Queries/By Area/Language & Cultural Equity/Students Needing ELL Testing.sql
@@ -0,0 +1,56 @@
+SELECT
+	Organization.ORGANIZATION_NAME AS SchoolName
+	,GradeLevel.VALUE_DESCRIPTION AS GradeLevel
+	,Student.SIS_NUMBER
+	,Person.LAST_NAME AS LastName
+	,Person.FIRST_NAME AS FirstName
+	,HomeLanguage.VALUE_DESCRIPTION AS HomeLanguage
+FROM
+	APS.PrimaryEnrollmentsAsOf(GETDATE()) AS Enroll
+	INNER JOIN
+	rev.EPC_STU AS Student
+	ON 
+	Enroll.STUDENT_GU = Student.STUDENT_GU
+
+	LEFT JOIN
+	OPENQUERY([SMAXDBPROD.APS.EDU.ACTD],'SELECT CAST(ID_NBR AS VARCHAR) AS ID_NBR FROM PR.APS.LCELatestEvaluationAsOf (GETDATE()) WHERE DST_NBR = 1') AS MostRecentTest
+	ON
+	Student.SIS_NUMBER = MostRecentTest.ID_NBR COLLATE DATABASE_DEFAULT
+
+	LEFT JOIN
+	OPENQUERY([SMAXDBPROD.APS.EDU.ACTD],'SELECT CAST(ID_NBR AS VARCHAR) AS ID_NBR, DECLND FROM PR.APS.LCEMostRecentTestDeclineAsOf (GETDATE()) WHERE DST_NBR = 1') AS Refusals
+	ON Student.SIS_NUMBER = Refusals.ID_NBR COLLATE DATABASE_DEFAULT
+
+	-- the rest of the joins is to accomidate the formatted columns
+	INNER JOIN
+	rev.REV_PERSON AS Person
+	ON 
+	Enroll.STUDENT_GU = Person.PERSON_GU
+
+	INNER JOIN
+	rev.REV_ORGANIZATION_YEAR AS OrgYear
+	ON 
+	Enroll.ORGANIZATION_YEAR_GU = OrgYear.ORGANIZATION_YEAR_GU
+
+	INNER JOIN
+	rev.REV_ORGANIZATION AS Organization
+	ON
+	OrgYear.ORGANIZATION_GU = Organization.ORGANIZATION_GU
+
+	INNER JOIN
+	APS.LookupTable('K12','GRADE') AS GradeLevel
+	ON
+	Enroll.GRADE = GradeLevel.VALUE_CODE
+
+	INNER JOIN
+	APS.LookupTable('K12','Language') AS HomeLanguage
+	ON
+	Student.HOME_LANGUAGE = HomeLanguage.VALUE_CODE
+WHERE
+	Student.HOME_LANGUAGE NOT IN ('00','54')
+	AND COALESCE(Refusals.DECLND,'N') != 'Y' -- No Declines
+	AND MostRecentTest.ID_NBR IS NULL -- No Tests
+	AND GradeLevel.VALUE_DESCRIPTION NOT IN ('P1', 'P2', 'PK')
+ORDER BY
+	Organization.ORGANIZATION_NAME
+	,GradeLevel.LIST_ORDER
