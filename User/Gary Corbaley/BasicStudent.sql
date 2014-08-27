cb5072149eb238645cfd4da9134f4c786886f861
Gary Corbaley <Gary.Corbaley@aps.edu>
2014-08-27 18:12:45 +0000

    Update line item comments
    git-svn-id: file:///var/repos/aps/Synergy/trunk@107 74b2d9da-2730-d14b-887d-b15a5e3d2562

--- User/Gary Corbaley/BasicStudent.sql
+++ User/Gary Corbaley/BasicStudent.sql
@@ -20,6 +20,7 @@ SELECT
 	,[Person].[MIDDLE_NAME]
 	,[Person].[BIRTH_DATE]
 	,[Person].[GENDER]
+	,[Language].[VALUE_DESCRIPTION] AS [HOME_LANGUAGE]
 	,[Student].[EXPECTED_GRADUATION_YEAR] AS [CLASS_OF]	
 	,[Person].[HISPANIC_INDICATOR]
 	,[Person].[HOME_ADDRESS_GU]
@@ -35,23 +36,28 @@ SELECT
 	,[MAIL_ADDRESS].[STATE] AS [MAIL_STATE]
 	,[MAIL_ADDRESS].[ZIP_5] AS [MAIL_ZIP]
 FROM
+	-- Get District and State ID Numbers
 	rev.EPC_STU AS [Student]		
 		
+	-- Get Personal Details
 	INNER JOIN
 	rev.REV_PERSON AS [Person]
 	ON
 	[Student].[STUDENT_GU] = [Person].[PERSON_GU]
 	
+	-- Get Home Language
 	LEFT OUTER JOIN
 	APS.LookupTable ('K12', 'Language') AS [Language]	
 	ON
 	[Student].[HOME_LANGUAGE] = [Language].[VALUE_CODE]
 	
+	-- Get Home Address
 	LEFT OUTER JOIN
 	rev.REV_ADDRESS AS [HOME_ADDRESS]
 	ON
 	[Person].[HOME_ADDRESS_GU] = [HOME_ADDRESS].[ADDRESS_GU]
 	
+	-- Get Mailing Address
 	LEFT OUTER JOIN
 	rev.REV_ADDRESS AS [MAIL_ADDRESS]
 	ON
