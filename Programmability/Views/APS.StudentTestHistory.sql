5fd1b4fbc810780b877049059a52ec3e2d0298b0
Brian Rieb <Brian.Rieb@aps.edu>
2014-09-02 15:37:31 +0000

    * View to easily see Student Test History
    git-svn-id: file:///var/repos/aps/Synergy/trunk@122 74b2d9da-2730-d14b-887d-b15a5e3d2562

---
+++ Programmability/Views/APS.StudentTestHistory.sql
@@ -0,0 +1,69 @@
+/**
+ * 
+ * $LastChangedBy: Brian Rieb
+ * $LastChangedDate: 08/29/2014 $
+ *
+ * Readable Student Test Information
+ * One Record Per Student per test
+ */
+
+IF NOT EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[APS].[StudentTestHistory]'))
+	EXEC ('CREATE VIEW APS.StudentTestHistory AS SELECT 0 AS DUMMY')
+GO
+
+ALTER VIEW APS.StudentTestHistory AS
+
+SELECT
+	Student.SIS_NUMBER
+	,Person.LAST_NAME
+	,Person.FIRST_NAME
+	,Test.TEST_NAME
+	,StudentTest.ADMIN_DATE
+	,Org.ORGANIZATION_NAME AS CurrentYearEnrollmentSchool
+	,GradeLevel.VALUE_DESCRIPTION AS CurrentYearEnrollmentGrade
+	,Enroll.ENTER_DATE AS CurrentYearEnrollmentEnterDate
+	,Enroll.LEAVE_DATE AS CurrentYearEnrollmentLeaveDate
+
+	,Student.STUDENT_GU
+	,Test.TEST_GU
+	,StudentTest.STUDENT_TEST_GU
+	,Enroll.STUDENT_SCHOOL_YEAR_GU
+FROM
+	rev.EPC_STU_TEST AS StudentTest
+	INNER JOIN
+	rev.EPC_TEST AS Test
+	ON
+	StudentTest.TEST_GU = Test.TEST_GU
+
+	INNER JOIN
+	rev.EPC_STU AS Student
+	ON
+	StudentTest.STUDENT_GU = Student.STUDENT_GU
+
+	INNER JOIN
+	rev.REV_PERSON AS Person
+	ON
+	StudentTest.STUDENT_GU = Person.PERSON_GU
+
+	LEFT JOIN
+	APS.PrimaryEnrollmentsAsOf(GETDATE()) AS Enroll
+	ON
+	StudentTest.STUDENT_GU = Enroll.STUDENT_GU
+
+	LEFT JOIN
+	rev.REV_ORGANIZATION_YEAR AS OrgYear
+	ON
+	Enroll.ORGANIZATION_YEAR_GU = OrgYear.ORGANIZATION_YEAR_GU
+
+	LEFT JOIN
+	rev.REV_ORGANIZATION AS Org
+	ON
+	OrgYear.ORGANIZATION_GU = Org.ORGANIZATION_GU
+
+	LEFT JOIN
+	APS.LookupTable('K12','Grade') AS GradeLevel
+	ON
+	Enroll.GRADE = GradeLevel.VALUE_CODE
+WHERE
+	Test.TEST_NAME = 'WAPT'
+	AND StudentTest.ADMIN_DATE > '2014-08-13'
