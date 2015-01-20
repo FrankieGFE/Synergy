7b23b3fe075b1aab07e509eab93bde57175fd27a
Debbie Chavez <Chavez_Deb@aps.edu>
2015-01-20 17:48:07 +0000

    Student Grades Section and All Tags
    git-svn-id: file:///var/repos/aps/Synergy/trunk@306 74b2d9da-2730-d14b-887d-b15a5e3d2562

---
+++ Programmability/Views/SectionAndAllTags.sql
@@ -0,0 +1,30 @@
+
+/**
+ * $Revision: 1 $
+ * $LastChangedBy: e104090 $
+ * $LastChangedDate: 2014-10-14 $
+ *
+ */
+
+ IF NOT EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[APS].[SectionsAndAllTags]'))
+	EXEC ('CREATE VIEW APS.SectionsAndAllTags AS SELECT 0 AS DUMMY')
+GO
+
+ALTER VIEW APS.SectionsAndAllTags AS
+
+
+SELECT 
+	SECTION_GU
+	,[ALSMA], [ALSMP],[ALS2W], [ALSED], [ALSSC], [ALSSS], [ALSSH], [ALSLA], [ALSES], [ALSOT], [ALSNV]
+
+ FROM
+		rev.UD_SECTION_TAG AS Tag
+
+	PIVOT
+		(
+		MAX(TAG)
+		FOR TAG IN ([ALSMA], [ALSMP],[ALS2W], [ALSED], [ALSSC], [ALSSS], [ALSSH], [ALSLA], [ALSES], [ALSOT], [ALSNV])
+		)
+	AS PivotedTable
+
+	GO
