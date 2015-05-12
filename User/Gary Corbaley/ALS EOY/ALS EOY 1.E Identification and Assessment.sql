81f392e3bd01f6c9c35ffaa392be8d2fde5be94b
Gary Corbaley <Gary.Corbaley@aps.edu>
2015-05-12 17:33:08 +0000

    git-svn-id: file:///var/repos/aps/Synergy/trunk@427 74b2d9da-2730-d14b-887d-b15a5e3d2562

--- User/Gary Corbaley/ALS EOY/ALS EOY 1.E Identification and Assessment.sql
+++ User/Gary Corbaley/ALS EOY/ALS EOY 1.E Identification and Assessment.sql
@@ -10,22 +10,22 @@ GO
 
 
 SELECT
-	[FILE1].[Code]
-	,[FILE1].[Cluster]
-	,[FILE1].[School]
-	,[FILE2].[PQ] AS [Req. 01B Current Year]
-	,[FILE1].[Last Year] AS [Req. 01B Prior Year]
-	,[FILE2].[Last Year NotElementary Schoolt] AS [Req. 01J Current Year]
-	,[FILE1].[YA] AS [Req. 01J Prior Year]
-	,[FILE2].[YA2] AS [Req. 01E Current Year]
-	,[FILE1].[PQ2] AS [Req. 01E Prior Year]
+	[PREVIOUS_FILE].[Code]
+	,[PREVIOUS_FILE].[Cluster]
+	,[PREVIOUS_FILE].[School]
+	,[CURRENT_FILE].[PQ] AS [Req. 01B Current Year]
+	,[PREVIOUS_FILE].[Last Year] AS [Req. 01B Prior Year]
+	,[CURRENT_FILE].[Last Year NotElementary Schoolt] AS [Req. 01J Current Year]
+	,[PREVIOUS_FILE].[YA] AS [Req. 01J Prior Year]
+	,[CURRENT_FILE].[YA2] AS [Req. 01E Current Year]
+	,[PREVIOUS_FILE].[PQ2] AS [Req. 01E Prior Year]
 FROM
 	OPENROWSET (
 			'Microsoft.ACE.OLEDB.12.0', 
 			'Text;Database=\\SynTempSSIS\Files\TempQuery\ALS EOY\;', 
 			--'SELECT * from Personal_Needs_Profile_Export_20141125_FULL_DISTRICT.csv'
 			'SELECT DISTINCT * from 1_E.csv' 
-		)AS [FILE1]
+		)AS [PREVIOUS_FILE]
 		
 	LEFT OUTER JOIN
 	OPENROWSET (
@@ -33,9 +33,9 @@ FROM
 			'Text;Database=\\SynTempSSIS\Files\TempQuery\ALS EOY\;', 
 			--'SELECT * from Personal_Needs_Profile_Export_20141125_FULL_DISTRICT.csv'
 			'SELECT DISTINCT * from 1_E.csv' 
-		)AS [FILE2]
+		)AS [CURRENT_FILE]
 	ON
-	[FILE1].[School] = [FILE2].[School]
+	[PREVIOUS_FILE].[School] = [CURRENT_FILE].[School]
 		
 		
 REVERT
