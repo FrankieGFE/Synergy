d0deedcb23b28a78c7cfca3fb784ace2a617ea7d
Sean Mcmurray <Sean.Mcmurray@aps.edu>
2015-06-16 14:17:54 +0000

    git-svn-id: file:///var/repos/aps/Synergy/trunk@476 74b2d9da-2730-d14b-887d-b15a5e3d2562

--- Packages/EdupointInterfaces/InterfacePackages/Destiny/CreateTempSummerTermsTable.sql
+++ Packages/EdupointInterfaces/InterfacePackages/Destiny/CreateTempSummerTermsTable.sql
@@ -17,9 +17,8 @@ select
 	 , tdef.EVENT_DATE
 from rev.rev_organization      org
 join rev.REV_ORGANIZATION_YEAR oyr   on oyr.ORGANIZATION_GU = org.ORGANIZATION_GU
-join rev.REV_YEAR              yr    on yr.SCHOOL_YEAR = (select school_year from rev.SIF_22_Common_CurrentYear)
-                                        and yr.EXTENSION = 'S'
-									    and yr.YEAR_GU = oyr.YEAR_GU
+join rev.REV_YEAR              yr    on yr.YEAR_GU = oyr.YEAR_GU
+									 AND yr.YEAR_GU IN (SELECT YEAR_GU FROM APS.YearDates WHERE GETDATE() BETWEEN YearDates.START_DATE AND YearDates.END_DATE)
 join rev.EPC_SCH_YR_TRM_DEF    tdef  on tdef.ORGANIZATION_YEAR_GU = oyr.ORGANIZATION_YEAR_GU
 join rev.EPC_SCH_YR_TRM_CODES  tcd   on tcd.SCHOOL_YEAR_TRM_DEF_GU = tdef.SCHOOL_YEAR_TRM_DEF_GU
 join rev.EPC_SCH_ATT_CAL_OPT   acalo on acalo.ORG_YEAR_GU = oyr.ORGANIZATION_YEAR_GU
