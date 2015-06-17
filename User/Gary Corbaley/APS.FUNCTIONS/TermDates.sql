fa485326018b8bc2586b07b5c0c971b0f088fa96
Gary Corbaley <Gary.Corbaley@aps.edu>
2015-06-17 21:41:02 +0000

    git-svn-id: file:///var/repos/aps/Synergy/trunk@505 74b2d9da-2730-d14b-887d-b15a5e3d2562

--- User/Gary Corbaley/APS.FUNCTIONS/TermDates.sql
+++ User/Gary Corbaley/APS.FUNCTIONS/TermDates.sql
@@ -14,9 +14,22 @@
  * Tables and Functions Referenced: APS.YearDates, EPC_SCH, REV_ORGANIZATION, REV_ORGANIZATION_YEAR, REV_YEAR, EPC_SCH_ATT_CAL_OPT, EPC_SCH_ATT_CAL, EPC_SCH_YR_TRM_DEF, EPC_SCH_YR_TRM_CODES, 
  */
 
-CREATE FUNCTION [APS].[TermDates]
-RETURNS TABLE
+CREATE FUNCTION [APS].[TermDates]()
+RETURNS @TERMS TABLE
+(
+	[SCHOOL_YEAR] INT NULL
+	,[EXTENSION] VARCHAR(1) NULL
+	,[SCHOOL_NAME] VARCHAR(100) NULL
+	,[TermCode] VARCHAR(5) NULL
+	,[TermName] VARCHAR(50) NULL
+	,[TermBegin] smalldatetime NULL
+	,[TermEnd] smalldatetime NULL
+	,[OrgYearGU] uniqueidentifier NULL
+	,[YEAR_GU] uniqueidentifier NULL
+	,[ORGANIZATION_GU] uniqueidentifier NULL
+)
 AS
+BEGIN
 
 DECLARE @retTerms TABLE 
 (
@@ -143,7 +156,7 @@ close schoolDefs
 deallocate schoolDefs
 
 -- Return the List of Term Definitions and add the school year and school name for user readability
-RETURN
+INSERT INTO @TERMS
 SELECT
 	[RevYear].[SCHOOL_YEAR]
 	,[RevYear].[EXTENSION]
@@ -176,4 +189,7 @@ FROM
 ORDER BY
 	[Organization].[ORGANIZATION_NAME]
 	
+RETURN
+
+END	
 GO
