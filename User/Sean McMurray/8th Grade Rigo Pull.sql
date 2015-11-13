2be1a47b05a247afccd5bb560de7d94ed8fd6b4a
Sean Mcmurray <Sean.Mcmurray@aps.edu>
2015-11-13 16:42:40 +0000

    git-svn-id: file:///var/repos/aps/Synergy/trunk@788 74b2d9da-2730-d14b-887d-b15a5e3d2562

---
+++ User/Sean McMurray/8th Grade Rigo Pull.sql
@@ -0,0 +1,58 @@
+--Created by e207878/Sean McMurray
+--Created on 10/1/15
+--Pulls Student Information for 5th and 8th graders
+
+SELECT
+
+    DISTINCT SIS_NUMBER AS [ID Number]
+	,LAST_NAME AS [Last Name]
+	,FIRST_NAME AS [First Name]
+	,MIDDLE_NAME AS [Middle Name]
+	,ADDRESS AS [Home Address]
+	,CITY AS [City]
+	,STATE AS [State]
+	,ZIP_5 AS [Zip Code]
+	,GRADE AS [Grade]
+	,PRIMARY_PHONE AS [Phone Number]
+	,SCHOOL_NAME AS [School Name]
+
+ 
+FROM
+APS.PrimaryEnrollmentDetailsAsOf ('10-29-15')   --Change this date based on how many days to pull (This pulled for the 120th day)
+INNER JOIN 
+rev.REV_PERSON ON APS.PrimaryEnrollmentDetailsAsOf.STUDENT_GU = rev.REV_PERSON.PERSON_GU
+INNER JOIN
+rev.REV_ADDRESS ON rev.REV_PERSON.HOME_ADDRESS_GU = rev.REV_ADDRESS.ADDRESS_GU
+INNER JOIN
+rev.EPC_STU ON APS.PrimaryEnrollmentDetailsAsOf.STUDENT_GU = rev.EPC_STU.STUDENT_GU
+
+--//*Change this to include different schools and grades*//
+
+WHERE
+
+	(GRADE = '08')
+	AND
+	(SCHOOL_NAME LIKE '%Washington%')
+	OR
+	(GRADE = '08')
+	AND
+	(SCHOOL_NAME LIKE '%Taft%')
+	OR
+	(GRADE = '08')
+	AND
+	(SCHOOL_NAME LIKE '%McKinley%')
+	OR
+	(GRADE = '08')
+	AND
+	(SCHOOL_NAME LIKE '%L.B.%')
+	OR
+	(GRADE = '08')
+	AND
+	(SCHOOL_NAME LIKE '%Jefferson%')
+	OR
+	(GRADE = '08')
+	AND
+	(SCHOOL_NAME LIKE '%Garfield%')
+	
+
+ORDER BY SCHOOL_NAME, GRADE;
