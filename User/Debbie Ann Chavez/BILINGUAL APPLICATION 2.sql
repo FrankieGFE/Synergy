208aae51c51353141558f2763bc46dd0390cc629
Debbie Chavez <Chavez_Deb@aps.edu>
2015-12-03 21:08:53 +0000

    git-svn-id: file:///var/repos/aps/Synergy/trunk@813 74b2d9da-2730-d14b-887d-b15a5e3d2562

--- User/Debbie Ann Chavez/BILINGUAL APPLICATION 2.sql
+++ User/Debbie Ann Chavez/BILINGUAL APPLICATION 2.sql
@@ -5,7 +5,7 @@ SELECT * FROM
 SELECT DISTINCT 
 	BEP.SCHOOL_CODE 
 	--THIS IS THE 40TH DAY DATE
-	,'2015-10-14' AS [DATE]
+	,'2015-12-01' AS [DATE]
 	,'' AS TITLE
 	,CASE
 		WHEN BEPProgramDescription = 'Enrichment' THEN 'Enrichment for FEP and English Speakers' 
@@ -114,7 +114,7 @@ SELECT DISTINCT
 	,'001' + BEP.SCHOOL_CODE AS SchoolCode_SchoolCode
 FROM 
 
-	APS.BilingualModelAndHoursDetailsAsOf('2015-10-14') AS BEP
+	APS.BilingualModelAndHoursDetailsAsOf('2015-12-01') AS BEP
 
 	INNER JOIN
 
@@ -134,7 +134,7 @@ FROM
 				,EnrollGrade
 				,ROW_NUMBER() OVER (PARTITION BY SCHOOL_CODE, BADGE_NUM,SectionID ORDER BY SectionID) AS RN
 			FROM 
-				APS.BilingualModelAndHoursDetailsAsOf('2015-10-14') AS BEP
+				APS.BilingualModelAndHoursDetailsAsOf('2015-12-01') AS BEP
 				) as t1
 			WHERE
 				RN = 1
@@ -298,7 +298,7 @@ UNION ALL
 SELECT DISTINCT 
 	BEP.SCHOOL_CODE 
 	--THIS IS THE 40TH DAY DATE
-	,'2015-10-14' AS [DATE]
+	,'2015-12-01' AS [DATE]
 	,'' AS TITLE
 	,CASE
 		WHEN BEPProgramDescription = 'Enrichment' THEN 'Enrichment for FEP and English Speakers' 
@@ -408,7 +408,7 @@ SELECT DISTINCT
 	,'001' + BEP.SCHOOL_CODE AS SchoolCode_SchoolCode
 FROM 
 
-	APS.BilingualModelAndHoursDetailsAsOf('2015-10-14') AS BEP
+	APS.BilingualModelAndHoursDetailsAsOf('2015-12-01') AS BEP
 
 	INNER JOIN
 
@@ -428,7 +428,7 @@ FROM
 				,EnrollGrade
 				,ROW_NUMBER() OVER (PARTITION BY SCHOOL_CODE, BADGE_NUM,SectionID ORDER BY SectionID) AS RN
 			FROM 
-				APS.BilingualModelAndHoursDetailsAsOf('2015-10-14') AS BEP
+				APS.BilingualModelAndHoursDetailsAsOf('2015-12-01') AS BEP
 				) as t1
 			WHERE
 				RN = 1
