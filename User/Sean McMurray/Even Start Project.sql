d756bf29c7dfae5541092394b6da05d1d15bae2e
Sean Mcmurray <Sean.Mcmurray@aps.edu>
2015-12-17 15:56:06 +0000

    git-svn-id: file:///var/repos/aps/Synergy/trunk@821 74b2d9da-2730-d14b-887d-b15a5e3d2562

---
+++ User/Sean McMurray/Even Start Project.sql
@@ -0,0 +1,39 @@
+SELECT
+
+    --DISTINCT
+	   STATE_STUDENT_NUMBER AS [State ID]
+       ,SIS_NUMBER AS [ID Number]
+       ,LAST_NAME AS [Last Name]
+       ,FIRST_NAME AS [First Name]
+       ,MIDDLE_NAME AS [Middle Name]
+	   ,SCHOOL_CODE AS [School Number]
+	   ,SCHOOL_NAME AS [School Name]
+	   ,GENDER
+	   ,RESOLVED_ETHNICITY_RACE AS [Ethnicity]
+	   ,PRIMARY_RACE_INDICATOR
+       ,ADDRESS AS [Home Address]
+       ,CITY AS [City]
+       ,STATE AS [State]
+       ,ZIP_5 AS [Zip Code]
+       ,GRADE AS [Grade]          
+
+FROM
+APS.PrimaryEnrollmentDetailsAsOf ('20151213')  AS PRIM --Change this date based on how many days to pull (This pulled for the 120th day)
+INNER JOIN 
+rev.REV_PERSON ON PRIM.STUDENT_GU = rev.REV_PERSON.PERSON_GU
+
+INNER JOIN
+rev.REV_ADDRESS ON rev.REV_PERSON.HOME_ADDRESS_GU = rev.REV_ADDRESS.ADDRESS_GU
+
+INNER JOIN
+rev.EPC_STU ON PRIM.STUDENT_GU = rev.EPC_STU.STUDENT_GU
+
+INNER JOIN
+REV.UD_STU AS UDSTU ON UDSTU.STUDENT_GU  = PRIM.STUDENT_GU
+
+LEFT JOIN
+
+WHERE
+UDSTU.EXCLUDE_BUSINESS = 'N'
+
+ORDER BY SCHOOL_NAME, GRADE;
