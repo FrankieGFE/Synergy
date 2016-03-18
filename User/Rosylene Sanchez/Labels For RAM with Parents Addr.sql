c879bbaf8c938050bca2bb59c1774b7342dd3139
Rosylene B Sanchez <sanchez_rb@aps.edu>
2016-03-18 20:43:29 +0000

    git-svn-id: file:///var/repos/aps/Synergy/trunk@943 74b2d9da-2730-d14b-887d-b15a5e3d2562

--- User/Rosylene Sanchez/Labels For RAM with Parents Addr.sql
+++ User/Rosylene Sanchez/Labels For RAM with Parents Addr.sql
@@ -8,7 +8,7 @@
 * INCLUDE:  Separate rows per parent, show wheather student has a 2015 or 2016 enrollment
 *
 * Tables Referenced: rev.EPC_STU, rev.EPC_STU_PARENT, rev.REV_PERSON, rev.REV_ADDRESS
-* Views:  
+* Views:  APS.StudentEnrollmentDetails
 */
 
 EXECUTE AS login='QueryFileUser' 
@@ -22,58 +22,65 @@ FROM   OPENROWSET ( 'Microsoft.ACE.OLEDB.12.0',
                     'SELECT * FROM   Labels_For_RAM.csv' 
 					) AS [RAMLabel] 
 
-		LEFT JOIN
-		(
-		SELECT sis_number, 
-                parent_last_name, 
-                parent_first_name,
-				ADDRESS,
-				ENR15.SCHOOL_YEAR AS [2015 ENR],
-				ENR16.SCHOOL_YEAR AS [2016 ENR]
+		LEFT JOIN (SELECT sis_number, 
+						  parent_last_name, 
+						  parent_first_name,
+				          ADDRESS,
+						  CASE
+						    WHEN ENR15.SUMMER_WITHDRAWL_CODE IS NOT NULL THEN ' '
+							ELSE CAST(ENR15.SCHOOL_YEAR AS VARCHAR)
+						  END AS [2015-2016 ENR],
+						  CASE
+						    WHEN ENR16.SUMMER_WITHDRAWL_CODE IS NOT NULL THEN ' '
+							ELSE CAST(ENR16.SCHOOL_YEAR AS VARCHAR)
+						  END AS [2016-2017 ENR]
 				
-		FROM   rev.epc_stu AS STU	--HAS SIS_NUMBER, STUDENT_GU,
+				   FROM   rev.epc_stu AS STU	--HAS SIS_NUMBER, STUDENT_GU,
 
-				INNER JOIN (SELECT SPAR.student_gu, 
-									parent_gu, 
-									PPER.last_name  AS PARENT_LAST_NAME, 
-									PPER.first_name AS PARENT_FIRST_NAME,
-									CASE
-										WHEN ADDR.ADDRESS is not null THEN ADDR.ADDRESS + ', ' + ADDR.CITY +', '+ ADDR.STATE + ', ' + ADDR.ZIP_5
-										ELSE ADDR2.ADDRESS + ', ' + ADDR2.CITY + ', ' + ADDR2.STATE + ', ' + ADDR2.ZIP_5
-									END AS ADDRESS
+						  INNER JOIN (SELECT SPAR.student_gu, 
+									         parent_gu, 
+									         PPER.last_name  AS PARENT_LAST_NAME, 
+									         PPER.first_name AS PARENT_FIRST_NAME,
+											 CASE
+											    WHEN ADDR.ADDRESS is not null THEN
+												ADDR.ADDRESS + ', ' + ADDR.CITY +', '+ 
+												ADDR.STATE + ', ' + ADDR.ZIP_5
+												ELSE ADDR2.ADDRESS + ', ' + ADDR2.CITY +
+													', ' + ADDR2.STATE + ', ' + ADDR2.ZIP_5
+											 END AS ADDRESS
 							
-							FROM rev.epc_stu_parent AS spar	--STUDENT_GU, PARENT_GU
+									   FROM	 rev.epc_stu_parent AS spar	--STUDENT_GU, PARENT_GU
 
-								LEFT JOIN rev.rev_person pper	--MAIL_ADDRESS_GU, HOME_ADDRESS_GU
-								ON pper.person_gu = spar.parent_gu
+										     LEFT JOIN rev.rev_person pper --MAIL_ADDRESS_GU, HOME_ADDRESS_GU
+										     ON pper.person_gu = spar.parent_gu
 
-								LEFT JOIN rev.REV_ADDRESS AS ADDR	--ADDRESS_GU, MAIL_ADDDRESS_GU
-								ON PPER.MAIL_ADDRESS_GU = ADDR.ADDRESS_GU
+											 LEFT JOIN rev.REV_ADDRESS AS ADDR --ADDRESS_GU, MAIL_ADDDRESS_GU
+											 ON PPER.MAIL_ADDRESS_GU = ADDR.ADDRESS_GU
 
-								LEFT JOIN rev.REV_ADDRESS AS ADDR2  --ADDRESS_GU, HOME_ADDRESS_GU
-								ON PPER.HOME_ADDRESS_GU = ADDR2.ADDRESS_GU
+											 LEFT JOIN rev.REV_ADDRESS AS ADDR2 --ADDRESS_GU, HOME_ADDRESS_GU
+											 ON PPER.HOME_ADDRESS_GU = ADDR2.ADDRESS_GU
 
-							) AS STUPAR 
+							            ) AS STUPAR 
 				
-				ON STU.STUDENT_GU = STUPAR.STUDENT_GU
+										ON STU.STUDENT_GU = STUPAR.STUDENT_GU 
 
-				---------------------
-				LEFT JOIN APS.StudentEnrollmentDetails AS ENR15
-				ON
-			    ENR15.SCHOOL_YEAR = '2015'	--2015-2016
-				AND ENR15.EXTENSION = 'R'
-				AND STU.STUDENT_GU = ENR15.STUDENT_GU
+					  ---------------------
+					  LEFT JOIN APS.StudentEnrollmentDetails AS ENR15
+							 ON ENR15.SCHOOL_YEAR = '2015'	--2015-2016
+						        AND ENR15.EXTENSION = 'R'
+								--AND ENR15.SUMMER_WITHDRAWL_CODE IS NULL
+						        AND STU.STUDENT_GU = ENR15.STUDENT_GU
 
-				LEFT JOIN APS.StudentEnrollmentDetails AS ENR16
-				ON
-			    ENR16.SCHOOL_YEAR = '2016'	--2014-2016
-				AND ENR16.EXTENSION = 'R'
-				AND STU.STUDENT_GU = ENR16.STUDENT_GU
-				----------------------
+					  LEFT JOIN APS.StudentEnrollmentDetails AS ENR16
+						     ON ENR16.SCHOOL_YEAR = '2016'	--2015-2016
+						        AND ENR16.EXTENSION = 'R'
+								--AND ENR16.SUMMER_WITHDRAWL_CODE IS NULL
+						        AND STU.STUDENT_GU = ENR16.STUDENT_GU
+					 ----------------------
 
-	   ) AS PARENT
+				) AS PARENT
 
-	   ON RAMLabel.SIS_NUMBER = PARENT.SIS_NUMBER
+	       ON RAMLabel.SIS_NUMBER = PARENT.SIS_NUMBER
 
 REVERT 
 
