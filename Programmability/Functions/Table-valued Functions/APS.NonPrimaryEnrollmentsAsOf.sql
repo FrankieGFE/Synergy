97f7f1d22fd945ad3754de1cfb6ab5ae403b2b66
Debbie Chavez <Chavez_Deb@aps.edu>
2016-04-18 15:12:04 +0000

    git-svn-id: file:///var/repos/aps/Synergy/trunk@985 74b2d9da-2730-d14b-887d-b15a5e3d2562

---
+++ Programmability/Functions/Table-valued Functions/APS.NonPrimaryEnrollmentsAsOf.sql
@@ -0,0 +1,35 @@
+
+/****** Object:  UserDefinedFunction [APS].[PrimaryEnrollmentsAsOf]    Script Date: 04/18/2016 08:43:18 ******/
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE FUNCTION [APS].[NonPrimaryEnrollmentsAsOf](@AsOfDate DATE)
+RETURNS TABLE
+AS
+RETURN	
+
+
+
+--DECLARE @AsOfDate DATE = GETDATE()
+	SELECT
+		[ENROLLMENTS].*
+	FROM
+		APS.StudentEnrollmentDetails AS [ENROLLMENTS]
+		
+		INNER JOIN
+		APS.YearDates AS [YearDates]
+		ON
+		[ENROLLMENTS].[YEAR_GU] = [YearDates].[YEAR_GU]		
+
+	WHERE
+		[ENROLLMENTS].[EXCLUDE_ADA_ADM] IS NOT NULL
+		AND @AsOfDate BETWEEN [YearDates].START_DATE AND [YearDates].END_DATE
+		AND @AsOfDate BETWEEN [ENROLLMENTS].ENTER_DATE AND COALESCE([ENROLLMENTS].LEAVE_DATE, [YearDates].END_DATE)
+		
+GO
+
+
