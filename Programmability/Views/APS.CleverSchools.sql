24ef1f42f59d910a4da2fefe75feee9baadf4ef5
Debbie Chavez <Chavez_Deb@aps.edu>
2016-07-19 21:23:29 +0000

    git-svn-id: file:///var/repos/aps/Synergy/trunk@1153 74b2d9da-2730-d14b-887d-b15a5e3d2562

---
+++ Programmability/Views/APS.CleverSchools.sql
@@ -0,0 +1,62 @@
+/*
+	Created by Debbie Ann Chavez (ORIGINAL CODE FROM MLM/EDUPOINT)
+	Date 7/18/2016
+*/
+
+
+ALTER VIEW APS.CleverSchools AS
+
+SELECT
+         sch.SCHOOL_CODE                      AS [School_id]
+       , org.ORGANIZATION_NAME                AS [School_name]
+       , sch.STATE_SCHOOL_CODE                AS [State_id]
+       , oadr.STREET_NAME                     AS [School_address]
+       , oadr.CITY                            AS [School_city]
+       , oadr.STATE                           AS [School_state]
+       , oadr.ZIP_5                           AS [School_zip]
+       , org.PHONE                            AS [School_phone]
+      , CASE
+	       WHEN lgrd.VALUE_DESCRIPTION in ('P3', 'P4', '5C', '8C') THEN 'Prekindergarten'
+	       WHEN lgrd.VALUE_DESCRIPTION = 'K'                       THEN 'Kindergarten'
+	       WHEN lgrd.VALUE_DESCRIPTION in ('PG', 'Grad', '12+')    THEN 'Postgraduate'
+		   WHEN lgrd.VALUE_DESCRIPTION = '01'                      THEN '1'
+		   WHEN lgrd.VALUE_DESCRIPTION = '02'                      THEN '2'
+		   WHEN lgrd.VALUE_DESCRIPTION = '03'                      THEN '3'
+		   WHEN lgrd.VALUE_DESCRIPTION = '04'                      THEN '4'
+		   WHEN lgrd.VALUE_DESCRIPTION = '05'                      THEN '5'
+		   WHEN lgrd.VALUE_DESCRIPTION = '06'                      THEN '6'
+		   WHEN lgrd.VALUE_DESCRIPTION = '07'                      THEN '7'
+		   WHEN lgrd.VALUE_DESCRIPTION = '08'                      THEN '8'
+		   WHEN lgrd.VALUE_DESCRIPTION = '09'                      THEN '9'
+		   ELSE lgrd.VALUE_DESCRIPTION
+		  END                         	      AS [Low_Grade]
+      , CASE
+	       WHEN hgrd.VALUE_DESCRIPTION in ('P3', 'P4', '5C', '8C') THEN 'Prekindergarten'
+	       WHEN hgrd.VALUE_DESCRIPTION = 'K'                       THEN 'Kindergarten'
+	       WHEN hgrd.VALUE_DESCRIPTION in ('PG', 'Grad', '12+')    THEN 'Postgraduate'
+		   WHEN hgrd.VALUE_DESCRIPTION = '01'                      THEN '1'
+		   WHEN hgrd.VALUE_DESCRIPTION = '02'                      THEN '2'
+		   WHEN hgrd.VALUE_DESCRIPTION = '03'                      THEN '3'
+		   WHEN hgrd.VALUE_DESCRIPTION = '04'                      THEN '4'
+		   WHEN hgrd.VALUE_DESCRIPTION = '05'                      THEN '5'
+		   WHEN hgrd.VALUE_DESCRIPTION = '06'                      THEN '6'
+		   WHEN hgrd.VALUE_DESCRIPTION = '07'                      THEN '7'
+		   WHEN hgrd.VALUE_DESCRIPTION = '08'                      THEN '8'
+		   WHEN hgrd.VALUE_DESCRIPTION = '09'                      THEN '9'
+		   ELSE hgrd.VALUE_DESCRIPTION
+		  END                       	      AS [High_Grade]
+       , prns.FIRST_NAME+ ' ' +prns.LAST_NAME AS [Principal]
+       , prns.EMAIL                           AS [Principal Email]
+FROM   rev.REV_ORGANIZATION           org
+       JOIN rev.REV_ORGANIZATION_YEAR oyr  ON oyr.ORGANIZATION_GU = org.ORGANIZATION_GU
+                                              AND oyr.YEAR_GU = (select YEAR_GU from rev.SIF_22_Common_CurrentYearGU)
+       JOIN rev.EPC_SCH               sch  ON sch.ORGANIZATION_GU = oyr.ORGANIZATION_GU
+	   LEFT JOIN rev.REV_ADDRESS      oadr ON oadr.ADDRESS_GU = org.ADDRESS_GU
+	   LEFT JOIN rev.REV_PERSON       prns ON prns.PERSON_GU = sch.PRINCIPAL_STAFF_GU
+	   LEFT JOIN rev.SIF_22_Common_GetLookupValues('K12', 'GRADE') lgrd on lgrd.VALUE_CODE 
+	                                                                       = (select min(grade) from rev.EPC_SCH_GRADE sg
+																		      where sg.ORGANIZATION_YEAR_GU = oyr.ORGANIZATION_YEAR_GU)
+	   LEFT JOIN rev.SIF_22_Common_GetLookupValues('K12', 'GRADE') hgrd on hgrd.VALUE_CODE 
+	                                                                       = (select max(grade) from rev.EPC_SCH_GRADE sg
+																		      where sg.ORGANIZATION_YEAR_GU = oyr.ORGANIZATION_YEAR_GU
+																			  and cast(sg.grade as int) <= 230)
---
+++ Programmability/Views/APS.CleverSections.sql
@@ -0,0 +1,68 @@
+
+/*
+	Created by Debbie Ann Chavez (ORIGINAL CODE FROM MLM/EDUPOINT)
+	Date 7/18/2016
+*/
+
+
+ALTER VIEW APS.CleverSections AS
+
+
+SELECT
+         sch.SCHOOL_CODE                                  AS [School_id]
+       , sch.SCHOOL_CODE+'_'+sect.SECTION_ID              AS [Section_id]
+       , stf.BADGE_NUM                                    AS [Teacher_id]
+       , crs.COURSE_TITLE + ' - ' +
+	     sch.SCHOOL_CODE  + '_' +
+		 sect.SECTION_ID  + ' - ' +
+		 'Period:' + CAST(sect.PERIOD_BEGIN as char(3))   AS [Name]
+      , CASE
+	       --WHEN grd.VALUE_DESCRIPTION in ('P3', 'P4', '5C', '8C') THEN 'Prekindergarten'
+		   WHEN grd.VALUE_DESCRIPTION = 'P1'                      THEN 'P1'
+		   WHEN grd.VALUE_DESCRIPTION = 'P2'                      THEN 'P2'
+		   WHEN grd.VALUE_DESCRIPTION = 'PK'                      THEN 'PK'
+	       WHEN grd.VALUE_DESCRIPTION = 'K'                       THEN 'K'
+	       --WHEN grd.VALUE_DESCRIPTION in ('PG', 'Grad', '12+')    THEN 'Postgraduate'
+		   WHEN grd.VALUE_DESCRIPTION = '01'                      THEN '1'
+		   WHEN grd.VALUE_DESCRIPTION = '02'                      THEN '2'
+		   WHEN grd.VALUE_DESCRIPTION = '03'                      THEN '3'
+		   WHEN grd.VALUE_DESCRIPTION = '04'                      THEN '4'
+		   WHEN grd.VALUE_DESCRIPTION = '05'                      THEN '5'
+		   WHEN grd.VALUE_DESCRIPTION = '06'                      THEN '6'
+		   WHEN grd.VALUE_DESCRIPTION = '07'                      THEN '7'
+		   WHEN grd.VALUE_DESCRIPTION = '08'                      THEN '8'
+		   WHEN grd.VALUE_DESCRIPTION = '09'                      THEN '9'
+		   ELSE grd.VALUE_DESCRIPTION
+		  END                                             AS [Grade]
+       , crs.COURSE_TITLE                                 AS [Course_name]
+       , crs.COURSE_ID                                    AS [Course_number]
+       , sect.PERIOD_BEGIN                                AS [Period]
+       , dept.VALUE_DESCRIPTION                           AS [Subject]
+       , sect.TERM_CODE                                   AS [Term_name]
+	   --, CONVERT(VARCHAR(10), trms.TermStart, 101)        AS [Term_start]
+	   --, CONVERT(VARCHAR(10), trms.TermEnd, 101)          AS [Term_end] 
+FROM   rev.EPC_SCH_YR_SECT            sect 
+       JOIN rev.EPC_SCH_YR_CRS        scrs ON scrs.SCHOOL_YEAR_COURSE_GU = sect.SCHOOL_YEAR_COURSE_GU
+	   JOIN rev.REV_ORGANIZATION_YEAR oyr  ON oyr.ORGANIZATION_YEAR_GU   = scrs.ORGANIZATION_YEAR_GU
+                                              and oyr.YEAR_GU            = (select YEAR_GU from rev.SIF_22_Common_CurrentYearGU)
+       JOIN rev.EPC_SCH               sch  ON sch.ORGANIZATION_GU        = oyr.ORGANIZATION_GU
+       JOIN rev.EPC_CRS               crs  ON crs.COURSE_GU              = scrs.COURSE_GU
+	   JOIN rev.EPC_STAFF_SCH_YR      stfy ON stfy.STAFF_SCHOOL_YEAR_GU  = sect.STAFF_SCHOOL_YEAR_GU
+	   JOIN rev.EPC_STAFF             stf  ON stf.STAFF_GU               = stfy.STAFF_GU
+       JOIN rev.REV_PERSON            stfp ON stfp.PERSON_GU             = stfy.STAFF_GU
+	   JOIN
+	   (select 
+       t.OrgGU
+     , t.SchoolYear
+	 , t.TermCode
+	 , TermBegin
+	 , TermEnd
+from rev.SIF_22_TermInfo() t 
+     join rev.REV_ORGANIZATION_YEAR oyr on oyr.ORGANIZATION_GU = t.OrgGU
+	                                       and oyr.YEAR_GU     = (select year_gu from rev.SIF_22_Common_CurrentYearGU)) AS trms	
+		
+			
+		ON trms.OrgGu                 = oyr.ORGANIZATION_GU
+	                                          AND trms.TermCode          = sect.TERM_CODE
+       LEFT JOIN rev.SIF_22_Common_GetLookupValues('K12', 'GRADE') grd on grd.VALUE_CODE = sect.GRADE_RANGE_LOW
+	   LEFT JOIN rev.SIF_22_Common_GetLookupValues('K12,CourseInfo', 'Department') dept on dept.VALUE_CODE = crs.DEPARTMENT
