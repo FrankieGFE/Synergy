d9e4d660ed6862d6f45b9393fc032270ce2f612f
Frank Garcia <garcia_fr@aps.edu>
2016-07-26 17:32:16 +0000

    git-svn-id: file:///var/repos/aps/Synergy/trunk@1179 74b2d9da-2730-d14b-887d-b15a5e3d2562

---
+++ Programmability/Views/APS.Pearson_EasyBridge_Student.sql
@@ -0,0 +1,44 @@
+/**
+ * 
+ * $LastChangedBy: Gary Corbaley
+ * $LastChangedDate: 8/24/2015
+ *
+ * Request By: Andy Gutierrez
+ * InitialRequestDate: 08/19/2015
+ * 
+ * Initial Request: Pull all active students for Pearson EasyBridge extract
+ *
+ * Description: 
+ * One Record Per Student
+ *
+ * Tables Referenced: 
+ */
+ CREATE VIEW APS.Pearson_EasyBridge_Student
+ AS
+ 
+SELECT
+	[STUDENT].[SIS_NUMBER] AS [student_code]
+	,[STUDENT].[LAST_NAME] AS [last_name]
+	,[STUDENT].[FIRST_NAME] AS [first_name]
+	,[STUDENT].[MIDDLE_NAME] AS [middle_name]
+	,[STUDENT].[GENDER] AS [gender_code]
+	,CONVERT(VARCHAR(4),YEAR([STUDENT].[BIRTH_DATE])) + '-' + RIGHT('00' + CONVERT(VARCHAR(2),DATEPART(MM,[STUDENT].[BIRTH_DATE])),2) + '-' + RIGHT('00' + CONVERT(VARCHAR(2),DATEPART(DD,[STUDENT].[BIRTH_DATE])),2) AS [dob]
+	,[PERSON].[EMAIL] AS [email]
+	,[STUDENT].[SIS_NUMBER] AS [student_number]
+	,[STUDENT].[SIS_NUMBER] AS [federated_id]
+	
+FROM
+	APS.PrimaryEnrollmentDetailsAsOf(GETDATE()) [ENROLLMENT]
+	
+	INNER JOIN
+	APS.BasicStudent AS [STUDENT]
+	ON
+	[ENROLLMENT].[STUDENT_GU] = [STUDENT].[STUDENT_GU]
+	
+	INNER JOIN
+	rev.REV_PERSON AS [PERSON]
+	ON
+	[ENROLLMENT].[STUDENT_GU] = [PERSON].[PERSON_GU]
+	
+WHERE
+	[ENROLLMENT].[GRADE] IN ('06','07','08')
