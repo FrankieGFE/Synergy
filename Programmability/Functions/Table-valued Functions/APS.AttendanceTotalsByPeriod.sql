c32d4048f51e72deea9b728717fbe9ca7eccac26
APS\e104090 <chavez_deb@aps.edu>
2017-02-14 17:35:36 -0700

    no message

---
+++ Programmability/Functions/Table-valued Functions/APS.AttendanceTotalsByPeriod.sql
@@ -0,0 +1,135 @@
+
+
+
+CREATE FUNCTION [APS].[AttendanceTotalsByPeriod](@AsOfDate DATETIME)
+RETURNS TABLE
+AS
+RETURN
+
+
+SELECT 
+	SIS_NUMBER
+	,SCHOOL_CODE
+	,EXCLUDE_ADA_ADM
+	,PERIOD_BEGIN
+	,CAST(COUNT(*) AS DECIMAL(5,2)) AS [Unexcused Count For Day]
+FROM ( 
+--gets unique records by date first
+SELECT
+    [stu].[STUDENT_GU]
+    ,[stu].[SIS_NUMBER]
+    ,[sch].[SCHOOL_CODE]
+	,[ssy].EXCLUDE_ADA_ADM
+	,SECT.PERIOD_BEGIN
+	,CAL.CAL_DATE
+    --,CAST(COUNT(*) AS DECIMAL(5,2)) AS [Unexcused Count For Day]
+FROM
+    [rev].[EPC_STU_ATT_DAILY] AS [atd] WITH (NOLOCK)
+
+    INNER JOIN
+    [rev].[EPC_STU_ATT_PERIOD] AS [atp] WITH (NOLOCK)
+    ON
+    [atd].[DAILY_ATTEND_GU]=[atp].[DAILY_ATTEND_GU]
+
+    INNER JOIN
+    [rev].[EPC_STU_ENROLL] AS [enr] WITH (NOLOCK)
+    ON
+    [atd].[ENROLLMENT_GU]=[enr].[ENROLLMENT_GU]
+    AND [enr].[EXCLUDE_ADA_ADM] IS NULL
+
+    INNER JOIN
+    [rev].[EPC_STU_SCH_YR] AS [ssy] WITH (NOLOCK)
+    ON
+    [enr].[STUDENT_SCHOOL_YEAR_GU]=[ssy].[STUDENT_SCHOOL_YEAR_GU]
+    AND [ssy].[EXCLUDE_ADA_ADM] IS NULL
+
+    INNER JOIN
+    [rev].[REV_ORGANIZATION_YEAR] AS [oy] WITH (NOLOCK)
+    ON
+    [ssy].[ORGANIZATION_YEAR_GU]=[oy].[ORGANIZATION_YEAR_GU]
+
+    INNER JOIN
+    [rev].[EPC_SCH] AS [sch] WITH (NOLOCK)
+    ON
+    [oy].[ORGANIZATION_GU]=[sch].[ORGANIZATION_GU]
+
+	INNER JOIN
+	[APS].[YearDates] AS [yr] WITH (NOLOCK)
+	ON
+	[oy].[YEAR_GU]=[yr].[YEAR_GU]
+	AND (GETDATE() BETWEEN [yr].[START_DATE] AND [yr].[END_DATE])
+	AND [yr].EXTENSION = 'R'
+    LEFT JOIN
+    [rev].[EPC_SCH_ATT_CAL] AS [cal] WITH (NOLOCK)
+    ON
+    [oy].[ORGANIZATION_YEAR_GU]=[cal].[SCHOOL_YEAR_GU] 
+    AND [atd].[ABS_DATE]=[cal].[CAL_DATE]
+
+    LEFT JOIN
+    [rev].[EPC_SCH_YR_BELL_SCHED] AS [bs] WITH (NOLOCK)
+    ON
+    [oy].[ORGANIZATION_YEAR_GU]=[bs].[ORGANIZATION_YEAR_GU]
+    AND [cal].[BELL_SCHEDULE]=[bs].[BELL_SCHEDULE_CODE]
+
+    LEFT JOIN
+    [rev].[EPC_SCH_YR_BELL_SCHED_PER] AS [bel] WITH (NOLOCK)
+    ON
+    [bs].[BELL_SCHEDULE_GU]=[bel].[BELL_SCHEDULE_GU]
+    AND [atp].[BELL_PERIOD]=[bel].[BELL_PERIOD]
+
+    LEFT JOIN
+    [rev].[EPC_CODE_ABS_REAS_SCH_YR] AS [abry] WITH (NOLOCK)
+    ON
+    [atp].[CODE_ABS_REAS_GU]=[abry].[CODE_ABS_REAS_SCH_YEAR_GU]
+
+    LEFT JOIN
+    [rev].[EPC_CODE_ABS_REAS] AS [abr] WITH (NOLOCK)
+    ON
+    [abry].[CODE_ABS_REAS_GU]=[abr].[CODE_ABS_REAS_GU]
+
+    INNER JOIN
+    [rev].[EPC_STU_CLASS] AS [scls] WITH (NOLOCK)
+    ON
+    [ssy].[STUDENT_SCHOOL_YEAR_GU]=[scls].[STUDENT_SCHOOL_YEAR_GU]
+    AND
+    ([atd].[ABS_DATE]<=[scls].[LEAVE_DATE] OR [scls].[LEAVE_DATE] IS NULL)
+    AND 
+    [atd].[ABS_DATE]>=[scls].[ENTER_DATE]
+
+    INNER JOIN
+    [rev].[EPC_SCH_YR_SECT] AS [sect] WITH (NOLOCK)
+    ON
+    [scls].[SECTION_GU]=[sect].[SECTION_GU]
+    AND
+    ([atp].[BELL_PERIOD]=[sect].[PERIOD_BEGIN] OR [atp].[BELL_PERIOD]=[sect].[PERIOD_END])
+    
+    INNER JOIN
+    [rev].[EPC_STU] AS [stu] WITH (NOLOCK)
+    ON
+    [ssy].[STUDENT_GU]=[stu].[STUDENT_GU]
+
+	INNER JOIN 
+	REV.EPC_SCH_YR_OPT AS SETUP
+	ON
+	SETUP.ORGANIZATION_YEAR_GU = OY.ORGANIZATION_YEAR_GU
+
+WHERE
+    [abr].[TYPE] IN ('UNE', 'EXC')
+  	AND SETUP.SCHOOL_ATT_TYPE IN ('P', 'B')
+
+GROUP BY
+    [stu].[STUDENT_GU]
+    ,[stu].[SIS_NUMBER]
+    ,[sch].[SCHOOL_CODE]
+	,[ssy].EXCLUDE_ADA_ADM
+	,SECT.PERIOD_BEGIN
+	,CAL.CAL_DATE
+
+) AS T1
+
+GROUP BY
+   [STUDENT_GU]
+    ,[SIS_NUMBER]
+    ,[SCHOOL_CODE]
+	,EXCLUDE_ADA_ADM
+	,PERIOD_BEGIN
--- Queries/By Area/STARS/Attendance/Step1PullMemberDays.sql
+++ Queries/By Area/STARS/Attendance/Step1PullMemberDays.sql
@@ -1,139 +1,79 @@
-/*
 
-CREATE TABLE STUDENT_SCHOOL_MEMBERDAYS
+--DROP TABLE dbo.STUDENT_SCHOOL_MEMBERDAYS_120D
+--CREATE TABLE dbo.STUDENT_SCHOOL_MEMBERDAYS_120D
+
+--(
+--	SIS_NUMBER VARCHAR(9)
+--	,FIRST_NAME VARCHAR(50)
+--	,LAST_NAME VARCHAR(50)
+--	,GRADE VARCHAR(2)
+--	,ENTER_DATE VARCHAR(10)
+--	,LEAVE_DATE VARCHAR(10)
+--	,SCHOOL_YEAR VARCHAR(4)
+--	,SCHOOL_CODE VARCHAR(4)
+--	,EXCLUDE_ADA_ADM VARCHAR (3)
+--	,MEMBERDAYS VARCHAR (3)
+--)
+
+
+INSERT INTO dbo.STUDENT_SCHOOL_MEMBERDAYS_120D
+
+select
+  stu.SIS_NUMBER                            as SIS_NUMBER
+, per.FIRST_NAME                            as FirstName
+, per.LAST_NAME                             as LastName
+, grd.VALUE_DESCRIPTION                     as GradeLevel
+, convert(varchar(10), enr.enter_date, 101) as EnterDate
+, case 
+     when enr.LEAVE_DATE is not null then convert(varchar(10), enr.LEAVE_DATE, 101) 
+     when '20170208' > copt.END_DATE   then convert(varchar(10), copt.END_DATE, 101) 
+     else convert(varchar(10), '20170208', 101)
+  end 
+as LeaveDate
+, yr.SCHOOL_YEAR                            as SchoolYear
+, sch.SCHOOL_CODE                           as SchoolCode
+,ENR.EXCLUDE_ADA_ADM						AS EXCLUDE_ADA_ADM
+, MembershipDays.MbDays                     as MembershipDays
+
+from rev.EPC_STU               stu
+join rev.REV_PERSON            per  on  per.PERSON_GU              = stu.STUDENT_GU
+join rev.EPC_STU_SCH_YR        ssy  on  ssy.STUDENT_GU             = stu.STUDENT_GU
+join rev.EPC_STU_ENROLL        enr  on  enr.STUDENT_SCHOOL_YEAR_GU = ssy.STUDENT_SCHOOL_YEAR_GU
+join rev.REV_ORGANIZATION_YEAR oyr  on  oyr.ORGANIZATION_YEAR_GU   = ssy.ORGANIZATION_YEAR_GU
+join rev.REV_ORGANIZATION      org  on  org.ORGANIZATION_GU        = oyr.ORGANIZATION_GU
+join rev.REV_YEAR              yr   on  yr.YEAR_GU                 = oyr.YEAR_GU
+join rev.EPC_SCH               sch  on  sch.ORGANIZATION_GU        = oyr.ORGANIZATION_GU
+join rev.EPC_SCH_ATT_CAL_OPT   copt on  copt.ORG_YEAR_GU           = oyr.ORGANIZATION_YEAR_GU
+left join rev.SIF_22_Common_GetLookupValues('K12', 'GRADE') grd on grd.VALUE_CODE = ssy.GRADE
+
+join 
 (
- startDate datetime,
- endDate datetime,
- org_year_gu VARCHAR (200),
- STUID VARCHAR (9),
- ORIGENTERDATE DATE,
- SCHOOL VARCHAR (50),
- MEMBER_DAYS VARCHAR (3),
-SCHOOL_CODE VARCHAR (3)
- )
- */
+select distinct
+       stu.STUDENT_GU
+     , ct.OrgGu
+     , ct.OrgYrGu
+     , sum(cast(ct.Daytype as int)) over (partition by ct.OrgGu, ct.OrgYrGu, stu.student_gu) MbDays
+from rev.EPC_STU               stu
+join rev.EPC_STU_SCH_YR        ssy  on ssy.STUDENT_GU             = stu.STUDENT_GU
+join rev.EPC_STU_ENROLL        enr  on enr.STUDENT_SCHOOL_YEAR_GU = ssy.STUDENT_SCHOOL_YEAR_GU
+
+join rev.REV_ORGANIZATION_YEAR oyr  on oyr.ORGANIZATION_YEAR_GU   = ssy.ORGANIZATION_YEAR_GU
+join rev.REV_ORGANIZATION      org  on org.ORGANIZATION_GU        = oyr.ORGANIZATION_GU
+join rev.EPC_SCH               sch  on sch.ORGANIZATION_GU        = oyr.ORGANIZATION_GU
+join rev.EPC_SCH_ATT_CAL_OPT   copt on copt.ORG_YEAR_GU           = oyr.ORGANIZATION_YEAR_GU
+join dbo.CalDayTable       ct   on ct.OrgGU                   = oyr.ORGANIZATION_GU
+                                       and ct.OrgYrGu             = oyr.ORGANIZATION_YEAR_GU
+where ct.caldate between (CASE WHEN ENR.ENTER_DATE <= '20161202' THEN '20161202' 
+								WHEN ENR.ENTER_DATE > '20161202' THEN ENR.ENTER_DATE ELSE '' END) 
+ and coalesce(enr.leave_date, '20170208')
 
 
-declare @startDate datetime;
-declare @endDate datetime;
-declare @org_year_gu uniqueidentifier;
-DECLARE @STUID VARCHAR (9);
-DECLARE @ORIGENTERDATE DATE;
-DECLARE @SCHOOL VARCHAR (50);
-DECLARE @SCHOOL_CODE VARCHAR (3)
+--AND SIS_NUMBER = 100039718
+--and sch.SCHOOL_CODE = @School
+) AS MembershipDays     
 
-deallocate x_cur;  --after the first time running you'll need this to avoid warning
-declare x_cur cursor for
-
-
-
-SELECT                    --ENR.ENTER_DATE 
-					--'2016-02-03' 
-					case when enter_date < '20160811' then ENTER_DATE else ENTER_DATE end as new_enter_date
-                    ,CASE WHEN LEAVE_DATE IS NULL THEN '20161012' ELSE LEAVE_DATE END AS NEWLEAVE
-                    ,ORGANIZATION_YEAR_GU
-					,ENR.SIS_NUMBER
-					,ENTER_DATE
-					,SCHOOL_NAME
-					,SCHOOL_CODE
-
-             FROM
-             APS.StudentEnrollmentDetails AS ENR
-			 INNER JOIN 
-			 rev.EPC_STU AS STU
-			 ON 
-			 STU.STUDENT_GU = ENR.STUDENT_GU
-             WHERE
-             SCHOOL_YEAR = '2016'
-             AND EXTENSION = 'R'
-             AND SCHOOL_CODE BETWEEN '200' AND '599'
-             AND SCHOOL_CODE != '533'
-             AND (ENTER_DATE <= '20161012' AND LEAVE_DATE IS NULL OR LEAVE_DATE BETWEEN '20160811' AND '20161012')
-
-open x_cur
- fetch next from x_cur into
-@startDate,
-@endDate,
-@org_year_gu,
-@STUID
-,@ORIGENTERDATE
-,@SCHOOL
-,@SCHOOL_CODE
---select 'first', @startDate as startdate, @endDate as enddate, @org_year_gu org_gu
-
---print @@fetch_status
-while @@FETCH_STATUS = 0 
-begin
-
-INSERT INTO STUDENT_SCHOOL_MEMBERDAYS
-
-SELECT
-	@startDate
-	,@endDate
-	,@org_year_gu
-	,@STUID
-	,@ORIGENTERDATE
-	,@SCHOOL
-	
-,CASE 
--- if the passed date is large than the last day, then return zero as invalid
-WHEN @endDate < @startDate THEN 0
--- else we calculate the difference between the passed date and the first day an each orgYear
--- stripping out weekends and holidays
-ELSE
-(DATEDIFF(dd, (CASE WHEN @startDate<START_DATE THEN START_DATE ELSE @startDate END), @endDate) + 1) -- Number of days difference between 2 dates (start date and day looked up)
--(DATEDIFF(wk, (CASE WHEN @startDate<START_DATE THEN START_DATE ELSE @startDate END), @endDate) * 2) -- Number of weeks difference (*2) - removes weekends
--(CASE WHEN DATENAME(dw, @endDate) IN ('Saturday','Sunday') THEN 1 ELSE 0 END) -- subtract one if the end day is on a (weekend) Not sure what we want to do hear
-- NumNonDays -- number of holidays 
-END
-AS MemberDay
-,@SCHOOL_CODE
-
-FROM
--- this subselect gives you first and last day of calendar along with number of holidays before passed date
-(
-SELECT
-CalOption.ORG_YEAR_GU
-,CASE WHEN @startDate<MIN(CalOption.START_DATE) THEN MIN(CalOption.START_DATE) ELSE @startDate END AS START_DATE
-,CASE WHEN @endDate<MIN(CalOption.END_DATE) THEN MIN(CalOption.END_DATE) ELSE @endDate END AS END_DATE
-,COUNT(*) * CASE WHEN SchoolCal.SCHOOL_YEAR_GU IS NULL THEN 0 ELSE 1 END AS NumNonDays
-FROM
-REV.EPC_SCH_ATT_CAL_OPT AS CalOption
-
-LEFT JOIN
-REV.EPC_SCH_ATT_CAL AS SchoolCal
-ON
-SchoolCal.SCHOOL_YEAR_GU = CalOption.ORG_YEAR_GU
-AND SchoolCal.HOLIDAY IN ('Hol','Sta', 'Oth','Non','Ema', 'Vac')
-
-AND CAL_DATE <= @endDate
-AND CAL_DATE >= @startDate
-where ORG_YEAR_GU = @org_year_gu
-GROUP BY
-CalOption.ORG_YEAR_GU
--- used to identify calendars with no holidays between start and passed date
-,CASE WHEN SchoolCal.SCHOOL_YEAR_GU IS NULL THEN 0 ELSE 1 END
-)NumDays
-
---print 'after'
---print @org_year_gu
---print @startDate
---print @endDate
-
- fetch next from x_cur into
-@startDate,
-@endDate,
-@org_year_gu,
-@STUID
-,@ORIGENTERDATE
-,@SCHOOL
-,@SCHOOL_CODE
---select @startDate as startdate, @endDate as enddate, @org_year_gu org_gu
-
-end
-
-close x_cur
-go
-
+on  MembershipDays.OrgGu            = oyr.ORGANIZATION_GU
+                                    and MembershipDays.OrgYrGU                 = oyr.ORGANIZATION_YEAR_GU
+                                    and MembershipDays.STUDENT_GU              = stu.STUDENT_GU
 
 
