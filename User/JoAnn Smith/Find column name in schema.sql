fe9055962b061b35a0707bd0f6ed512e517cb074
APS\e211395 <joann.smith@aps.edu>
2017-03-21 08:25:07 -0600

    column name change

--- User/JoAnn Smith/Find column name in schema.sql
+++ User/JoAnn Smith/Find column name in schema.sql
@@ -5,6 +5,7 @@ SCHEMA_NAME(schema_id) AS schema_name,
 c.name AS column_name
 FROM sys.tables AS t
 INNER JOIN sys.columns c ON t.OBJECT_ID = c.OBJECT_ID
-WHERE c.name LIKE '%MEMBER_DAYS%'
+WHERE c.name LIKE '%PARENT%'
 ORDER BY schema_name, table_name;
+
 
