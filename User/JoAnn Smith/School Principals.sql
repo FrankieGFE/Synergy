1f97ffd0c8b60159fe9506ce69612e58f8f7a73c
APS\e211395 <joann.smith@aps.edu>
2017-03-21 08:24:26 -0600

    added more fields that Jude wanted

--- User/JoAnn Smith/School Principals.sql
+++ User/JoAnn Smith/School Principals.sql
@@ -1,5 +1,9 @@
+declare @school uniqueidentifier = '725E6DD0-62B7-40AD-9ADE-63793563E8EC'
 
-select
+
+
+	select
+	org.ORGANIZATION_GU,
 	 ORG.ORGANIZATION_NAME as [School Name],
 	 isnull(SCH.SCHOOL_CODE, '') as [School Code],
 	 isnull(per.LAST_NAME + ', ' + per.FIRST_NAME, '') as [Principal Name],
@@ -8,7 +12,9 @@ select
 	 ADDR.STATE as [State],
 	 ADDR.ZIP_5 as [Zip],
 	 isnull(org.PHONE, '') as [Phone],
-	 sch.LIVE_IN_GENESIS as [Live in Synergy]
+	 isnull(sch.LIVE_IN_GENESIS, '') as [Live in Synergy],
+	 sch.ALT_School as [Alternative School],
+	 isnull(sch.NCES_SCHOOL_NUM, '') as [NCES School Number]
 	 
 from
 	 rev.REV_ORGANIZATION ORG
@@ -24,7 +30,7 @@ JOIN
 	REV.REV_ADDRESS ADDR
 ON
 	ADDR.ADDRESS_GU = ORG.ADDRESS_GU
---WHERE
---	ORG.ORGANIZATION_GU LIKE @School
-
+WHERE
+	ORG.ORGANIZATION_GU LIKE @School
 ORDER BY ORGANIZATION_NAME
+
