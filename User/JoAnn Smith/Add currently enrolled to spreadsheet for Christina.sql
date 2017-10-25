5575406c11f12638af0f882dc31c7c98539654b0
APS\e211395 <joann.smith@aps.edu>
2017-10-25 13:20:55 -0600

    Christina needed to know which students from 2013-2014 were enrolled this year.

---
+++ User/JoAnn Smith/Add currently enrolled to spreadsheet for Christina.sql
@@ -0,0 +1,69 @@
+--read from text file
+EXECUTE AS LOGIN='QueryFileUser'
+GO
+
+;WITH STUDENTS_WITH_BALANCES
+AS
+(
+SELECT 
+            *
+			
+FROM
+       OPENROWSET (
+              'Microsoft.ACE.OLEDB.12.0', 
+              'Text;Database=\\SYNTEMPSSIS\Files\TempQuery;',
+              'SELECT * FROM SummerSchoolBalanceDue.csv')
+)
+,Currently_Enrolled
+as
+(
+select
+	e.STUDENT_GU,
+	e.SIS_NUMBER,
+	'Y' as CURRENTLY_ENROLLED
+from
+	rev.epc_stu e
+inner join
+	rev.epc_stu_sch_yr ssy
+on
+	e.STUDENT_GU= ssy.STUDENT_GU
+inner join
+	rev.REV_ORGANIZATION_YEAR oy
+on
+	ssy.ORGANIZATION_YEAR_GU = oy.ORGANIZATION_YEAR_GU
+inner join
+	rev.rev_year yr
+on
+	oy.YEAR_GU = yr.YEAR_GU
+where
+	yr.SCHOOL_YEAR = '2017' and EXTENSION = 'R'
+)
+--select * from Currently_Enrolled
+,Results
+as
+(
+SELECT
+	ROW_NUMBER() over (partition by S.[Perm Id] order by S.[Perm Id]) as rn,
+	S.[Student Name],
+	s.[Perm ID],
+	s.Gender,
+	s.Grade,
+	s.[Total Fees],
+	s.[Total Payments],
+	s.Credits,
+	s.Refunds,
+	s.Debits,
+	s.Balance,
+	s.[Refund Needed],
+	C.CURRENTLY_ENROLLED
+
+
+FROM
+	STUDENTS_WITH_BALANCES S
+left join
+	Currently_Enrolled c
+on
+	s.[Perm ID] = c.SIS_NUMBER
+)
+select * from Results where rn = 1 
+--select * from aps.PrimaryEnrollmentsAsOf(getdate()) where STUDENT_GU = 'BD865CE5-01D6-4E0D-AB6D-C9B2DC72A202'
