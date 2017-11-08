c57a9c53f69f807d0953a9d2c03b3da2c9dc039e
APS\e104090 <chavez_deb@aps.edu>
2017-11-07 17:26:41 -0700

    PGM-1405, Update5thDigit

---
+++ Programmability/Functions/Table-valued Functions/APS.LCEPGM1405.sql
@@ -0,0 +1,115 @@
+
+
+/*
+ *  CREATED BY DEBBIE ANN CHAVEZ
+ *	DATE 11/7/2017
+ *
+ * List of current students in a bilingual program that are missing a spanish evaluation
+ * or did not score proficient in a spansih evaluation
+ AND ARE IN A SPANISH LANGUAGE ARTS CLASS - THIS EXCLUDES NAVJAO AND ZUNI 
+ */
+
+
+
+ 
+ALTER FUNCTION [APS].[LCEPGM1405](@AsOfDate DATE)
+RETURNS TABLE
+AS
+RETURN
+
+
+SELECT
+	Organization.ORGANIZATION_NAME
+	,Organization.ORGANIZATION_GU
+	,GradeLevel.VALUE_DESCRIPTION AS GradeLevel
+	,GradeLevel.LIST_ORDER AS GradeOrder
+	,Student.SIS_NUMBER
+	,Person.LAST_NAME
+	,Person.FIRST_NAME
+	,Person.MIDDLE_NAME
+	,Person.BIRTH_DATE
+	,BEP.PROGRAM_CODE
+	,BEP.PROGRAM_INTENSITY
+	,SpanishEval.ADMIN_DATE
+	,SpanishEval.TEST_NAME
+	,SpanishEval.PERFORMANCE_LEVEL AS SLEVEL
+	,lu.VALUE_DESCRIPTION as PERFORMANCE_LEVEL
+
+FROM
+	APS.PrimaryEnrollmentsAsOf(@AsOfDate) AS Enroll
+	INNER JOIN
+	APS.LCEBilingualAsOf(@AsOfDate) AS BEP
+	ON
+	Enroll.STUDENT_GU = BEP.STUDENT_GU
+
+INNER JOIN 
+(
+SELECT DISTINCT SIS_NUMBER, STUDENT_GU
+ FROM 
+APS.ScheduleDetailsAsOf(@AsOfDate) AS SCH
+INNER JOIN 
+REV.EPC_CRS AS CRS
+ON
+SCH.COURSE_GU = CRS.COURSE_GU
+WHERE
+	(LEFT(STATE_COURSE_CODE,4) BETWEEN '1271' AND '1274'
+	OR LEFT(STATE_COURSE_CODE,4) BETWEEN '1202' AND '1203')
+	AND (CRS.COURSE_TITLE NOT LIKE '%NAVAJO%' OR CRS.COURSE_TITLE NOT LIKE '%ZUNI%') 
+
+) AS BEP2
+ON
+BEP.STUDENT_GU = BEP2.STUDENT_GU
+
+	LEFT JOIN
+	APS.LCELatestSpanishEvaluationAsOf(@AsOfDate) AS SpanishEval
+	ON
+	Enroll.STUDENT_GU = SpanishEval.STUDENT_GU
+
+
+	-- Make pretty table joins
+	INNER JOIN
+	rev.EPC_STU as STUDENT
+	ON
+	Enroll.STUDENT_GU = Student.STUDENT_GU
+
+	INNER JOIN
+	rev.REV_PERSON AS Person
+	ON
+	Enroll.STUDENT_GU = Person.PERSON_GU	
+
+	INNER JOIN
+	rev.REV_ORGANIZATION_YEAR AS OrgYear
+	ON
+	Enroll.ORGANIZATION_YEAR_GU = OrgYear.ORGANIZATION_YEAR_GU
+
+	INNER JOIN
+	rev.REV_ORGANIZATION AS Organization
+	ON
+	OrgYear.ORGANIZATION_GU = Organization.ORGANIZATION_GU
+
+	LEFT JOIN
+	APS.LookupTable('k12','Grade') AS GradeLevel
+	ON
+	Enroll.GRADE = GradeLevel.VALUE_CODE
+
+	left join
+	aps.LookupTable('K12.TestInfo', 'PERFORMANCE_LEVELS') as LU
+	on
+	SpanishEval.PERFORMANCE_LEVEL = lu.VALUE_CODE
+--WHERE
+--	--OrgYear.ORGANIZATION_GU LIKE @School
+--	--AND
+--	(
+--		SpanishEval.PERFORMANCE_LEVEL IS NULL
+--		OR SpanishEval.PERFORMANCE_LEVEL NOT IN ('FSP', 'C-PRO', 'ABVPR')
+--	)
+
+
+--ORDER BY
+--	Organization.ORGANIZATION_NAME
+--	,GradeLevel.LIST_ORDER
+--	,Person.LAST_NAME
+--	,Person.FIRST_NAME
+--	,Person.MIDDLE_NAME
+
+GO
